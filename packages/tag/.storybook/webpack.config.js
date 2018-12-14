521cb1680effd514040017f21e6a6994f22d151f
Aaron Bates <aaron@aaronbates.me>
2018-12-14 16:38:57 +0000

    feat: Webpack build for Core with SCSS tokenized

--- packages/tag/.storybook/webpack.config.js
+++ packages/tag/.storybook/webpack.config.js
@@ -1,9 +1,27 @@
+const sassJsonImporter = require('node-sass-json-importer');
+
 module.exports = (baseConfig, env, defaultConfig) => {
   // Extend defaultConfig as you need.
-
   defaultConfig.module.rules.push({
     test: /\.scss$/,
-    loaders: ['style-loader', 'css-loader', 'sass-loader'],
+    use: [
+      'style-loader',
+      {
+        loader: 'css-loader',
+        options: {
+          importLoaders: 2,
+        },
+      },
+      {
+        loader: 'postcss-loader',
+      },
+      {
+        loader: 'sass-loader',
+        options: {
+          importer: sassJsonImporter()
+        },
+      },
+    ],
   });
 
   return defaultConfig;
