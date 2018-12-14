fc05a3c943d2f01860cbbf42b007c098b42d83b5
Dan Denney <dd.subscriptions@gmail.com>
2018-12-14 13:41:56 -0500

    refactor(*): Move the catalog SCSS to src

--- catalog/index.js
+++ catalog/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Catalog } from 'catalog';
 import pages from './config/pages';
 import { logoSrc, theme, responsiveSizes } from './config/theme';
-import "./config/catalog.scss";
+import "../src/stylesheets/catalog.scss";
 
 const styles = [];
 const scripts = [];
