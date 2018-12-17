49fab8730201a41a5b24cb2d08db75a596eefd3e
Dan Denney <dd.subscriptions@gmail.com>
2018-12-17 10:07:48 -0500

    feat(design): Add a loop for utilities

--- catalog/pages/design-utilities/background.js
+++ catalog/pages/design-utilities/background.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Page } from 'catalog';
 
-import { COLORS } from '../../../packages/core/colors.js';
+import { colors } from '../../../packages/core/tokens.json';
 
-export default () => (
-  <Page>
-    {COLORS.map(name => (
-      <p>{ color }</p>
-    ))}
-  </Page>
-);
+export default () => {
+  const boom = Object.values(colors);
+  return(
+    <Page>
+
+      {boom.map(name => (
+        <li
+          className="dc-u-fx dc-u-m-24 dc-u-fx-fdc dc-u-fx-aic dc-u-w-192"
+          key={name}
+        >
+          <p>{name}</p>
+        </li>
+      ))}
+      
+    </Page>
+  )
+};
