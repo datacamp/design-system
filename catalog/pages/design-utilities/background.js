bfac7379251ac2acb8cbc4179110ff095919e7ec
Dan Denney <dd.subscriptions@gmail.com>
2019-01-16 13:41:56 -0500

    feat(design-utilities): Output the background class names

--- catalog/pages/design-utilities/background.js
+++ catalog/pages/design-utilities/background.js
@@ -9,21 +9,26 @@ export default () => {
   const obj = Object.keys(colors);
   return (
     <Page>
-      {obj.map(name => (
-        <li
-          className="dc-u-fx dc-u-fx-fdc dc-u-fx-aic dc-u-fx-jcc"
-          key={name}
-          style={{
-            background: getColor(name),
-            width: '192px',
-            height: '128px',
-            color: getContrastColor(name),
-          }}
-        >
-          <p>{name}</p>
-          <p>{`dc-${dashify(name)}`}</p>
-        </li>
-      ))}
+      <section>
+        <p>Background color utility classes are named using the format <code>.dc-u-bgc-color-name</code>. They can be used for building or customizing an element.</p>
+        
+        <ul class="dc-u-fx dc-u-fx-fww dc-u-m-none dc-u-p-none">
+          {obj.map(name => (
+            <li
+              className="dc-u-fx dc-u-fx-fdc dc-u-fx-aic dc-u-fx-jcc"
+              key={name}
+              style={{
+                background: getColor(name),
+                width: '256px',
+                height: '128px',
+                color: getContrastColor(name),
+              }}
+            >
+              <code>{`.dc-${dashify(name)}`}</code>
+            </li>
+          ))}
+        </ul>
+      </section>
     </Page>
   );
 };
