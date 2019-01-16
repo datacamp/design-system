5548c42945a8d47a2d2826cf87fce595073fc19a
Dan Denney <dd.subscriptions@gmail.com>
2019-01-16 15:44:20 -0500

    docs(design-utilities): Add colors

--- catalog/pages/design-utilities/color.js
+++ catalog/pages/design-utilities/color.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import { Page, HintSpecimen } from 'catalog';
 
+import { colors } from '../../../packages/core/tokens.json';
+import { getColor, getContrastColor } from '../../../packages/core/js/colors';
+import { dashify } from '../../helpers/dashify';
+
 export default () => {
+  const obj = Object.keys(colors);
   return (    
     <Page>
       <header>
-        <h2>Additive</h2>
-        <p>These classes add <code>color</code> to an element, so they can be used for building or customizing an element.</p>
+        <p>These classes add <code>color</code> to an element, so they can be used for building or customizing an element. They follow the format <code>.dc-u-color-[color-name]</code>.</p>
       </header>
 
-      <HintSpecimen warning>
-        Needs a custom loop and helper for class conversion.
-      </HintSpecimen>
+      <section class="dc-u-bgc-white dc-u-mt-24 dc-u-p-8">
+        <table className="table">
+          <thead>
+            <tr>
+              <th className="dc-u-w-1pc">Class Name</th>
+              <th>Description</th>
+              <th>Example</th>
+            </tr>
+          </thead>
+          <tbody>
+            {obj.map(name => (
+              <tr key={name}>
+                <td>
+                  <code className="dc-u-ws-nowrap">.dc-u-color-{dashify(name)}</code>
+                </td>
+                <td>
+                  <code>color: $dc-{dashify(name)};</code>
+                </td>
+                <td className={"dc-u-color-" + dashify(name)}>
+                  {dashify(name)}
+              </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
     </Page>
   );
 };
