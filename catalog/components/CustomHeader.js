457d753d66b2ff0ac6ef5f0f6535f02ebb3edd7a
Dan Denney <dd.subscriptions@gmail.com>
2019-01-31 15:37:41 -0500

    feat(catalog): Add a CustomHeader component

---
+++ catalog/components/CustomHeader.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CustomHeader = ({ section, subSection, description, extraClass }) => (
+  <header className="dc-u-bgg-chambray-cloudburst dc-u-pb-32 dc-u-pt-32 dc-u-pt-48 dc-u-ph-32">
+    <p className="dc-chapeau-title dc-u-color-secondary dc-u-mb-0">{section}</p>
+    <h1 className="dc-h3 dc-u-color-white dc-u-mv-4">{subSection}</h1>
+    <p className="dc-u-color-white dc-u-mv-none">{description}</p>
+  </header>
+);
+
+CustomHeader.defaultProps = {
+  extraClass: undefined,
+};
+
+CustomHeader.propTypes = {
+  section: PropTypes.string.isRequired,
+  subSection: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  extraClass: PropTypes.string,
+};
+
+export default CustomHeader;
--- catalog/pages/design-utilities/background.js
+++ catalog/pages/design-utilities/background.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Page } from 'catalog';
 
+import CustomHeader from '../../components/CustomHeader';
 import { colors } from '../../../packages/core/tokens.json';
 import { getColor, getContrastColor } from '../../../packages/core/js/colors';
 import dashify from '../../helpers/dashify';
@@ -8,27 +9,36 @@ import dashify from '../../helpers/dashify';
 export default () => {
   const obj = Object.keys(colors);
   return (
-    <Page>
-      <section>
-        <p>Background color utility classes are named using the format <code>.dc-u-bgc-[color-name]</code>. They can be used for building or customizing an element.</p>
-        
-        <ul class="dc-u-fx dc-u-fx-fww dc-u-m-none dc-u-p-none">
-          {obj.map(name => (
-            <li
-              className="dc-u-fx dc-u-fx-fdc dc-u-fx-aic dc-u-fx-jcc"
-              key={name}
-              style={{
-                background: getColor(name),
-                width: '256px',
-                height: '128px',
-                color: getContrastColor(name),
-              }}
-            >
-              <code>{`.dc-u-bgc-${dashify(name)}`}</code>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Page>
+    <main>
+      <CustomHeader
+        section="Design Utilities"
+        subSection="Background"
+        description="Apply or override background colors."
+      >
+      </CustomHeader>
+
+      <Page>
+        <section>
+          <p>Background color utility classes are named using the format <code>.dc-u-bgc-[color-name]</code>. They can be used for building or customizing an element.</p>
+
+          <ul class="dc-u-fx dc-u-fx-fww dc-u-m-none dc-u-p-none">
+            {obj.map(name => (
+              <li
+                className="dc-u-fx dc-u-fx-fdc dc-u-fx-aic dc-u-fx-jcc"
+                key={name}
+                style={{
+                  background: getColor(name),
+                  width: '256px',
+                  height: '128px',
+                  color: getContrastColor(name),
+                }}
+              >
+                <code>{`.dc-u-bgc-${dashify(name)}`}</code>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </Page>
+    </main>
   );
 };
