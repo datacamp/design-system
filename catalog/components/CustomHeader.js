e4b95890945c865b527e5274c18a65eef2655305
Dan Denney <dd.subscriptions@gmail.com>
2019-02-04 08:22:10 -0500

    refactor(catalog): Remove extraClass from CustomHeader

--- catalog/components/CustomHeader.js
+++ catalog/components/CustomHeader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CustomHeader = ({ section, subSection, description, extraClass }) => (
+const CustomHeader = ({ section, subSection, description }) => (
   <header className="dc-u-bgg-chambray-cloudburst dc-u-pb-32 dc-u-pt-32 dc-u-pt-48 dc-u-ph-32">
     <p className="dc-chapeau-title dc-u-color-secondary dc-u-mb-0">{section}</p>
     <h1 className="dc-h3 dc-u-color-white dc-u-mv-4">{subSection}</h1>
@@ -9,15 +9,10 @@ const CustomHeader = ({ section, subSection, description, extraClass }) => (
   </header>
 );
 
-CustomHeader.defaultProps = {
-  extraClass: undefined,
-};
-
 CustomHeader.propTypes = {
   section: PropTypes.string.isRequired,
   subSection: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  extraClass: PropTypes.string,
 };
 
 export default CustomHeader;
