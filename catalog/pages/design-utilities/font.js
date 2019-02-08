2e39f4c4c8ddfd8374872039c1020529c4e7f709
Dan Denney <dd.subscriptions@gmail.com>
2019-02-08 15:36:09 -0500

    refactor(design-utilities): Output a table for responsive utilities

--- catalog/pages/design-utilities/font.js
+++ catalog/pages/design-utilities/font.js
@@ -2,11 +2,8 @@ import React from 'react';
 import { Page } from 'catalog';
 
 import CustomHeader from '../../components/CustomHeader';
-import { breakpoints } from '../../../packages/core/tokens.json';
-import dashify from '../../helpers/dashify';
-import whitelist from '../../helpers/whitelist.js';
 
-const blacklist = ['ws', '4K', '5K', 'belowWs', 'below4K', 'below5K'];
+const sizes = ['base', 'micro', 'small', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
 
 export default () => {
   return (
@@ -108,270 +105,28 @@ export default () => {
           <p>Font size utilities can be applied to individual common breakpoints using @[size] appended to the end of the class.</p>
         </header>
 
-        <article className="dc-u-mh-4">
-          <h4>Base</h4>
-          <table className="table dc-u-w-auto">
-            <thead>
-              <tr>
-                <th className="dc-u-w-1pc">Class Name</th>
-                <th>Pixel Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.entries(whitelist(blacklist, breakpoints)).map(([name, value]) => (
-                <tr key={name}>
-                  <td>
-                    <code className="dc-u-ws-nowrap">{`.dc-u-fs-base@${dashify(name)}`}</code>
-                  </td>
-                  <td>
-                    {value}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </article>
-
-        <article className="dc-u-mh-4">
-          <h4>Micro</h4>
-          <table className="table dc-u-w-auto">
-            <thead>
-              <tr>
-                <th className="dc-u-w-1pc">Class Name</th>
-                <th>Pixel Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.entries(whitelist(blacklist, breakpoints)).map(([name, value]) => (
-                <tr key={name}>
-                  <td>
-                    <code className="dc-u-ws-nowrap">{`.dc-u-fs-micro@${dashify(name)}`}</code>
-                  </td>
-                  <td>
-                    {value}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </article>
-
-        <article className="dc-u-mh-4">
-          <h4>Small</h4>
-          <table className="table dc-u-w-auto">
-            <thead>
-              <tr>
-                <th className="dc-u-w-1pc">Class Name</th>
-                <th>Pixel Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.entries(whitelist(blacklist, breakpoints)).map(([name, value]) => (
-                <tr key={name}>
-                  <td>
-                    <code className="dc-u-ws-nowrap">{`.dc-u-fs-small@${dashify(name)}`}</code>
-                  </td>
-                  <td>
-                    {value}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </article>
-
-        <article className="dc-u-mh-4">
-          <h4>H1</h4>
-          <table className="table dc-u-w-auto">
-            <thead>
-              <tr>
-                <th className="dc-u-w-1pc">Class Name</th>
-                <th>Pixel Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.entries(whitelist(blacklist, breakpoints)).map(([name, value]) => (
-                <tr key={name}>
-                  <td>
-                    <code className="dc-u-ws-nowrap">{`.dc-u-fs-h1@${dashify(name)}`}</code>
-                  </td>
-                  <td>
-                    {value}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </article>
-
-        <article className="dc-u-mh-4">
-          <h4>H2</h4>
-          <table className="table dc-u-w-auto">
-            <thead>
-              <tr>
-                <th className="dc-u-w-1pc">Class Name</th>
-                <th>Pixel Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.entries(whitelist(blacklist, breakpoints)).map(([name, value]) => (
-                <tr key={name}>
-                  <td>
-                    <code className="dc-u-ws-nowrap">{`.dc-u-fs-h2@${dashify(name)}`}</code>
-                  </td>
-                  <td>
-                    {value}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </article>
-
-        <article className="dc-u-mh-4">
-          <h4>H3</h4>
-          <table className="table dc-u-w-auto">
-            <thead>
-              <tr>
-                <th className="dc-u-w-1pc">Class Name</th>
-                <th>Pixel Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.entries(whitelist(blacklist, breakpoints)).map(([name, value]) => (
-                <tr key={name}>
-                  <td>
-                    <code className="dc-u-ws-nowrap">{`.dc-u-fs-h3@${dashify(name)}`}</code>
-                  </td>
-                  <td>
-                    {value}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </article>
-
-        <article className="dc-u-mh-4">
-          <h4>H4</h4>
-          <table className="table dc-u-w-auto">
-            <thead>
-              <tr>
-                <th className="dc-u-w-1pc">Class Name</th>
-                <th>Pixel Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.entries(whitelist(blacklist, breakpoints)).map(([name, value]) => (
-                <tr key={name}>
-                  <td>
-                    <code className="dc-u-ws-nowrap">{`.dc-u-fs-h4@${dashify(name)}`}</code>
-                  </td>
-                  <td>
-                    {value}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </article>
-
-        <article className="dc-u-mh-4">
-          <h4>H5</h4>
-          <table className="table dc-u-w-auto">
-            <thead>
-              <tr>
-                <th className="dc-u-w-1pc">Class Name</th>
-                <th>Pixel Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.entries(whitelist(blacklist, breakpoints)).map(([name, value]) => (
-                <tr key={name}>
-                  <td>
-                    <code className="dc-u-ws-nowrap">{`.dc-u-fs-h5@${dashify(name)}`}</code>
-                  </td>
-                  <td>
-                    {value}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </article>
-
-        <article className="dc-u-mh-4">
-          <h4>H6</h4>
-          <table className="table dc-u-w-auto">
-            <thead>
-              <tr>
-                <th className="dc-u-w-1pc">Class Name</th>
-                <th>Pixel Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.entries(whitelist(blacklist, breakpoints)).map(([name, value]) => (
-                <tr key={name}>
-                  <td>
-                    <code className="dc-u-ws-nowrap">{`.dc-u-fs-h6@${dashify(name)}`}</code>
-                  </td>
-                  <td>
-                    {value}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </article>
-
-        <h4 className="dc-u-mb-8 dc-u-minw-100pc">Style</h4>
-        <table className="table">
+        <table>
           <thead>
             <tr>
-              <th className="dc-u-w-1pc">Class Name</th>
-              <th>Description</th>
-              <th>Example</th>
+              <th>Class Name</th>
+              <th>Responsive Modifiers</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td><code className="dc-u-ws-nowrap">.dc-u-fst-italic</code></td>
-              <td><code>font-style: italic;</code></td>
-              <td><p className="dc-u-m-none dc-u-fst-italic">Italic font style</p></td>
-            </tr>
-            <tr>
-              <td><code className="dc-u-ws-nowrap">.dc-u-fst-normal</code></td>
-              <td><code>font-style: normal;</code></td>
-              <td><p className="dc-u-m-none dc-u-fst-normal">Normal font style</p></td>
-            </tr>
-          </tbody>
-        </table>
-
-        <h4 className="dc-u-mb-8 dc-u-minw-100pc">Weight</h4>
-        <table className="table">
-          <thead>
-            <tr>
-              <th className="dc-u-w-1pc">Class Name</th>
-              <th>Description</th>
-              <th>Example</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td><code className="dc-u-ws-nowrap">.dc-u-fw-light</code></td>
-              <td><code>font-weight: 300;</code></td>
-              <td><p className="dc-u-m-none dc-u-fw-light">Light font weight</p></td>
-            </tr>
-            <tr>
-              <td><code className="dc-u-ws-nowrap">.dc-u-fw-regular</code></td>
-              <td><code>font-weight: 400;</code></td>
-              <td><p className="dc-u-m-none dc-u-fw-regular">Regular font weight</p></td>
-            </tr>
-            <tr>
-              <td><code className="dc-u-ws-nowrap">.dc-u-fw-bold</code></td>
-              <td><code>font-weight: 700;</code></td>
-              <td><p className="dc-u-m-none dc-u-fw-bold">Bold font weight</p></td>
-            </tr>
+            {sizes.map((name) => (
+              <tr className="dc-u-bt">
+                <td>
+                  <code>.dc-u-fs-{name}</code>
+                </td>
+                <td className="dc-u-ta-center">
+                  <span className="dc-icon dc-icon--size-18 dc-icon--green">
+                    <svg className="dc-icon__svg">
+                      <use xlinkHref="#checkmark_circle" />
+                    </svg>
+                  </span>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </Page>
