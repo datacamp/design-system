47fdbdc5f11327e3dd0c425058725c64c4f20e8f
Kristof Houben <kristof@datacamp.com>
2019-05-20 17:52:08 +0200

    feat: icon best practices

--- packages/docs/catalog-doc-site/catalog/config/sections/design.js
+++ packages/docs/catalog-doc-site/catalog/config/sections/design.js
@@ -4,6 +4,11 @@ export default {
   title: 'Design',
   pages: [
     {
+      path: '/design/icons',
+      title: 'Icons',
+      content: pageLoader(() => import('../../pages/design/icons.js')),
+    },
+    {
       path: '/design/logo',
       title: 'Logo',
       content: pageLoader(() => import('../../pages/design/logo.js')),
