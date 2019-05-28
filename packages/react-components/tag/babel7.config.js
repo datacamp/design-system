3eff60c7c861b52f6c0d233a4492f26fb76c65e9
Louis Bailey <louis@datacamp.com>
2019-05-28 11:41:53 +0200

    chore: use shared babel config in tag

--- packages/react-components/tag/babel7.config.js
+++ packages/react-components/tag/babel7.config.js
@@ -1,16 +1,3 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          browsers: ['last 2 versions', 'Firefox ESR', 'not IE < 11'],
-        },
-      },
-    ],
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-    '@emotion/babel-preset-css-prop',
-  ],
-  plugins: ['lodash', 'typescript-to-proptypes'],
+  presets: ['@datacamp/waffles-babel-preset'],
 };
