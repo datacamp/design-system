0c9ce56cd2ff99c0e029f9d7c5c99d5743c05a16
Louis Bailey <louis@datacamp.com>
2019-05-28 11:42:18 +0200

    chore: use shared babel config in icons

--- packages/react-components/icons/babel7.config.js
+++ packages/react-components/icons/babel7.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          browsers: ['last 2 versions', 'Firefox ESR', 'not IE < 11'],
-        },
-      },
-    ],
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-  ],
-  plugins: ['typescript-to-proptypes'],
+  presets: ['@datacamp/waffles-babel-preset'],
 };
