99a86e84b5891672752064a71ab5c0dec6281bd9
Louis Bailey <louis@datacamp.com>
2019-05-28 11:38:07 +0200

    feat: shared babel config
    BREAKING CHANGE: version 1

---
+++ packages/tools/babel-preset/index.js
@@ -0,0 +1,24 @@
+const presetEnv = require('@babel/preset-env').default;
+const presetReact = require('@babel/preset-react').default;
+const presetTypescript = require('@babel/preset-typescript').default;
+const presetCSSProp = require('@emotion/babel-preset-css-prop').default;
+const pluginLodash = require('babel-plugin-lodash');
+const pluginTypescriptToProptypes = require('babel-plugin-typescript-to-proptypes')
+  .default;
+
+module.exports = () => ({
+  presets: [
+    [
+      presetEnv,
+      {
+        targets: {
+          browsers: ['last 2 versions', 'Firefox ESR', 'not IE < 11'],
+        },
+      },
+    ],
+    presetReact,
+    presetTypescript,
+    presetCSSProp,
+  ],
+  plugins: [pluginLodash, pluginTypescriptToProptypes],
+});
