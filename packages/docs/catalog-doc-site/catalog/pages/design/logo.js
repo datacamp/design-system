1e36c17bd0a9a713108cd688841189a18b26bb0e
Kristof Houben <kristof@datacamp.com>
2019-06-11 17:14:40 +0200

    feat: added business logos

--- packages/docs/catalog-doc-site/catalog/pages/design/logo.js
+++ packages/docs/catalog-doc-site/catalog/pages/design/logo.js
@@ -16,7 +16,7 @@ export default () => {
 
       <Page>
         <section className="dc-u-fx dc-u-mb-16 dc-u-w-100pc">
-          <div className="dc-card dc-u-p-24" css={{ flex: '1 1 0' }}>
+          <div className="dc-card" css={{ flex: '1 1 0', padding: 24 }}>
             <h5 className="dc-u-m-none">Regular</h5>
             <div className="dc-card dc-card--bordered dc-u-p-24 dc-u-mv-16">
               <img
@@ -43,7 +43,10 @@ export default () => {
             </a>
           </div>
 
-          <div className="dc-card dc-u-p-24 dc-u-ml-16" css={{ flex: '1 1 0' }}>
+          <div
+            className="dc-card"
+            css={{ flex: '1 1 0', marginLeft: 16, padding: 24 }}
+          >
             <h5 className="dc-u-m-none">Inverted</h5>
             <div className="dc-u-bgc-primary-dark dc-u-brad-all dc-u-p-24 dc-u-mv-16">
               <img
@@ -70,7 +73,10 @@ export default () => {
             </a>
           </div>
 
-          <div className="dc-card dc-u-p-24 dc-u-ml-16" css={{ flex: '1 1 0' }}>
+          <div
+            className="dc-card"
+            css={{ flex: '1 1 0', marginLeft: 16, padding: 24 }}
+          >
             <h5 className="dc-u-m-none">Monochrome</h5>
             <div className="dc-card dc-card--bordered dc-u-p-24 dc-u-mv-16">
               <img
@@ -99,7 +105,7 @@ export default () => {
         </section>
 
         <section className="dc-u-fx dc-u-w-100pc">
-          <div className="dc-card dc-u-p-24" css={{ flex: '1 1 0' }}>
+          <div className="dc-card" css={{ flex: '1 1 0', padding: 24 }}>
             <h5 className="dc-u-m-none">Icon</h5>
             <div className="dc-card dc-card--bordered dc-u-p-24 dc-u-mv-16">
               <img
@@ -126,7 +132,10 @@ export default () => {
             </a>
           </div>
 
-          <div className="dc-card dc-u-p-24 dc-u-ml-16" css={{ flex: '1 1 0' }}>
+          <div
+            className="dc-card"
+            css={{ flex: '1 1 0', marginLeft: 16, padding: 24 }}
+          >
             <h5 className="dc-u-m-none">Icon Monochrome</h5>
             <div className="dc-card dc-card--bordered dc-u-p-24 dc-u-mv-16">
               <img
@@ -142,6 +151,94 @@ export default () => {
             <a
               className="dc-btn dc-btn--inline dc-u-mt-8"
               href="/logo/icon-monochrome.zip"
+            >
+              Download EPS, SVG, PNG
+              <span className="dc-icon dc-icon--size-12 dc-icon--flex dc-u-ml-8">
+                <svg className="dc-icon__svg">
+                  <use xlinkHref="#download" />
+                </svg>
+              </span>
+            </a>
+          </div>
+          <div
+            className="dc-card"
+            css={{
+              flex: '1 1 0',
+              marginLeft: 16,
+              padding: 24,
+              visibility: 'hidden',
+            }}
+          />
+        </section>
+
+        <h4 className="dc-u-mt-32 dc-u-mb-16">Business Logo&apos;s</h4>
+
+        <section className="dc-u-fx dc-u-w-100pc">
+          <div className="dc-card" css={{ flex: '1 1 0', padding: 24 }}>
+            <h5 className="dc-u-m-none">Regular</h5>
+            <div className="dc-card dc-card--bordered dc-u-p-24 dc-u-mv-16">
+              <img
+                src="/logo/business-regular.svg"
+                alt="DataCamp Business Logo – Regular"
+                className="dc-u-d-b"
+                css={{ margin: '0 auto' }}
+              />
+            </div>
+            <a
+              className="dc-btn dc-btn--inline dc-u-mt-8"
+              href="/logo/business-regular.zip"
+            >
+              Download EPS, SVG, PNG
+              <span className="dc-icon dc-icon--size-12 dc-icon--flex dc-u-ml-8">
+                <svg className="dc-icon__svg">
+                  <use xlinkHref="#download" />
+                </svg>
+              </span>
+            </a>
+          </div>
+
+          <div
+            className="dc-card"
+            css={{ flex: '1 1 0', marginLeft: 16, padding: 24 }}
+          >
+            <h5 className="dc-u-m-none">Inverted</h5>
+            <div className="dc-u-bgc-primary-dark dc-u-brad-all dc-u-p-24 dc-u-mv-16">
+              <img
+                src="/logo/business-inverted.svg"
+                alt="DataCamp Business Logo – Inverted"
+                className="dc-u-d-b"
+                css={{ margin: '0 auto' }}
+              />
+            </div>
+            <a
+              className="dc-btn dc-btn--inline dc-u-mt-8"
+              href="/logo/business-inverted.zip"
+            >
+              Download EPS, SVG, PNG
+              <span className="dc-icon dc-icon--size-12 dc-icon--flex dc-u-ml-8">
+                <svg className="dc-icon__svg">
+                  <use xlinkHref="#download" />
+                </svg>
+              </span>
+            </a>
+          </div>
+
+          <div
+            className="dc-card"
+            css={{ flex: '1 1 0', marginLeft: 16, padding: 24 }}
+          >
+            <h5 className="dc-u-m-none">Monochrome</h5>
+            <div className="dc-u-bgc-primary-dark dc-u-brad-all dc-u-p-24 dc-u-mv-16">
+              <img
+                src="/logo/business-monochrome.svg"
+                alt="DataCamp Business Logo – Monochrome"
+                className="dc-u-d-b"
+                css={{ margin: '0 auto' }}
+              />
+            </div>
+            <a
+              className="dc-btn dc-btn--inline dc-u-mt-8"
+              href="/logo/business-monochrome.zip"
             >
               Download EPS, SVG, PNG
               <span className="dc-icon dc-icon--size-12 dc-icon--flex dc-u-ml-8">
