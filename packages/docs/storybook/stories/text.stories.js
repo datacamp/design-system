d7a89535e34c15dcb8ef5793251e8bc1d7f3dd20
Louis Bailey <louis@datacamp.com>
2019-06-13 13:10:23 +0200

    feat: stories for Strong component

--- packages/docs/storybook/stories/text.stories.js
+++ packages/docs/storybook/stories/text.stories.js
@@ -1,19 +1,37 @@
 import './styles.css';
 
-import { Text } from '@datacamp/waffles-text';
+import { Strong, Text } from '@datacamp/waffles-text';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 
-storiesOf('waffles-text', module).add('Text Component', () => (
-  <>
-    <Text>This is the default text</Text>
-    <br />
-    <Text css={{ color: 'blue' }}>
-      This has additional style added using a css prop
-    </Text>
-    <br />
-    <Text className="example-class">
-      This has additional style added using a className
-    </Text>
-  </>
-));
+storiesOf('waffles-text', module)
+  .add('Text Component', () => (
+    <>
+      <Text>This is the default text</Text>
+      <br />
+      <Text css={{ color: 'blue' }}>
+        This has additional style added using a css prop
+      </Text>
+      <br />
+      <Text className="example-class">
+        This has additional style added using a className
+      </Text>
+      <br />
+      <Text>
+        This has <Strong>these words</Strong> made strong
+      </Text>
+    </>
+  ))
+  .add('Strong Component', () => (
+    <>
+      <Strong>This is the default strong text</Strong>
+      <br />
+      <Strong css={{ color: 'blue' }}>
+        This has additional style added using a css prop
+      </Strong>
+      <br />
+      <Strong className="example-class">
+        This has additional style added using a className
+      </Strong>
+    </>
+  ));
