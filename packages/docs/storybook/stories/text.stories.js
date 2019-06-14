79e3056ca6ee7fff37ca03981a734a91b60c661c
Louis Bailey <louis@datacamp.com>
2019-06-14 15:44:47 +0200

    feat: add stories for Paragraph component

--- packages/docs/storybook/stories/text.stories.js
+++ packages/docs/storybook/stories/text.stories.js
@@ -1,6 +1,7 @@
 import './styles.css';
 
-import { Strong, Text } from '@datacamp/waffles-text';
+import { Paragraph, Strong, Text } from '@datacamp/waffles-text';
+import { color } from '@datacamp/waffles-tokens/lib/future-tokens.json';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 
@@ -9,7 +10,7 @@ storiesOf('waffles-text', module)
     <>
       <Text>This is the default text</Text>
       <br />
-      <Text css={{ color: 'blue' }}>
+      <Text css={{ color: color.opaque.primary.value.hex }}>
         This has additional style added using a css prop
       </Text>
       <br />
@@ -26,12 +27,46 @@ storiesOf('waffles-text', module)
     <>
       <Strong>This is the default strong text</Strong>
       <br />
-      <Strong css={{ color: 'blue' }}>
+      <Strong css={{ color: color.opaque.primary.value.hex }}>
         This has additional style added using a css prop
       </Strong>
       <br />
       <Strong className="example-class">
         This has additional style added using a className
       </Strong>
+    </>
+  ))
+  .add('Paragraph Component', () => (
+    <>
+      <Paragraph>
+        This is a paragraph containing a string child. Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Donec a enim quis nisi sollicitudin
+        tincidunt in nec leo. Quisque mattis pretium nulla, id malesuada nisi
+        viverra consectetur. Donec auctor dapibus nisl sit amet tempor. Integer
+        nec diam sit amet sem sollicitudin consectetur. Vestibulum velit turpis,
+        egestas eget massa non, consectetur volutpat quam. Sed eu mi eget sem
+        euismod iaculis. Aenean a ante mattis orci dapibus pulvinar. Quisque
+        convallis arcu eu neque tincidunt condimentum.
+      </Paragraph>
+      <Paragraph>
+        This is a paragraph containing a some Text and Strong children. Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit. Donec a enim quis
+        nisi sollicitudin tincidunt in nec leo. <Strong>Quisque mattis</Strong>{' '}
+        pretium nulla, id malesuada nisi viverra consectetur. Donec auctor
+        dapibus nisl sit amet tempor. Integer nec diam sit amet sem sollicitudin
+        consectetur.{' '}
+        <Text css={{ color: color.opaque.primary.value.hex }}>
+          Vestibulum velit turpis
+        </Text>
+        , egestas eget massa non, consectetur volutpat quam. Sed eu mi eget sem
+        euismod iaculis. Aenean a ante mattis orci dapibus pulvinar. Quisque
+        convallis arcu eu neque tincidunt condimentum.
+      </Paragraph>
+      <Paragraph css={{ color: color.opaque.primary.value.hex }}>
+        This has additional style added using a css prop
+      </Paragraph>
+      <Paragraph className="example-class">
+        This has additional style added using a className
+      </Paragraph>
     </>
   ));
