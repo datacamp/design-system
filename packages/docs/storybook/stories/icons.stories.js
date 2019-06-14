f2a9de363f4a223a3a84263cba1a6de0094a0c81
Louis Bailey <louis@datacamp.com>
2019-06-14 11:49:13 +0200

    feat: change icon stories to be per component

--- packages/docs/storybook/stories/icons.stories.js
+++ packages/docs/storybook/stories/icons.stories.js
@@ -1,26 +1,14 @@
 import * as Icons from '@datacamp/waffles-icons';
-import styled from '@emotion/styled';
-import { color, select } from '@storybook/addon-knobs';
+import { color } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 
-const Container = styled.div`
-  display: 'grid',
-  grid-template-columns: 'auto auto auto',
-  width: '100%',
-  place-items: 'center',
-`;
-
-storiesOf('waffles-icons', module).add('All Icons', () => (
-  <Container>
-    {Object.entries(Icons).map(([name, Component]) => (
-      <p key={name}>
-        {name}{' '}
-        <Component
-          size={select('size', [12, 18, 24], 18)}
-          color={color('color', 'currentColor')}
-        />
-      </p>
-    ))}
-  </Container>
-));
+Object.entries(Icons).forEach(([name, Component]) => {
+  storiesOf('waffles-icons', module).add(name, () => (
+    <>
+      <Component size={12} color={color('color', 'currentColor')} />{' '}
+      <Component size={18} color={color('color', 'currentColor')} />{' '}
+      <Component size={24} color={color('color', 'currentColor')} />
+    </>
+  ));
+});
