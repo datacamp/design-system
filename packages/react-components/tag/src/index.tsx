c75e6430fed436e941ae7bee3514eeb29ae7a34b
Louis Bailey <louis@datacamp.com>
2019-06-19 10:20:05 +0200

    chore: fix import order linting

--- packages/react-components/tag/src/index.tsx
+++ packages/react-components/tag/src/index.tsx
@@ -2,7 +2,7 @@ import './injectGlobalLato';
 
 import { getColor, getContrastColor } from '@datacamp/waffles-core';
 import tokens from '@datacamp/waffles-tokens/lib/future-tokens.json';
-import { SerializedStyles, css } from '@emotion/core';
+import { css, SerializedStyles } from '@emotion/core';
 import styled from '@emotion/styled';
 import { isNil } from 'lodash';
 import React from 'react';
