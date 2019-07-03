590dba4aa113b2216155c2dbb0e4ad1da4256e4b
Louis Bailey <louis@datacamp.com>
2019-07-03 10:45:34 +0200

    feat: deprecate old heading component
    BREAKING CHANGE: This removes everything from this package. This version 
    will only throw an error if imported. Please use the Heading component 
    from the @datacamp/waffles-text package instead.

--- packages/react-components/heading/src/index.js
+++ packages/react-components/heading/src/index.js
@@ -1,25 +1,3 @@
-import cx from 'classnames';
-import PropTypes from 'prop-types';
-import React from 'react';
-
-const Heading = ({ children, element: elementProp, extraClass }) => {
-  const Element = elementProp;
-  return (
-    <Element className={cx(`dc-${elementProp}`, extraClass)}>
-      {children}
-    </Element>
-  );
-};
-
-Heading.defaultProps = {
-  element: 'h2',
-  extraClass: undefined,
-};
-
-Heading.propTypes = {
-  children: PropTypes.string.isRequired,
-  element: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
-  extraClass: PropTypes.string,
-};
-
-export default Heading;
+throw new Error(
+  '@datacamp/waffles-heading has been deprecated. Please use the Heading component from @datacamp/waffles-text instead.'
+);
