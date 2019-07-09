e6338ced1ad0aa1b7b452de8ea2eea09f18c0842
Louis Bailey <louis@datacamp.com>
2019-07-09 16:21:17 +0200

    fix: remove hashes from snapshot tests

--- packages/stylesheets/core/test/core.spec.js
+++ packages/stylesheets/core/test/core.spec.js
@@ -1,11 +1,27 @@
 const dirTree = require('directory-tree');
 const fs = require('fs');
 const omitDeep = require('omit-deep-lodash');
+const _ = require('lodash');
+
+// remove hash from filename so snapshots are diffed rather than recreated
+const replaceFileName = fileName =>
+  fileName.replace(/(waffles)\.(\w+)(\.css)/, '$1$3');
 
 describe('Core', () => {
   it('has correct file structure', () => {
     const dir = dirTree('lib');
-    expect(omitDeep(dir, 'size')).toMatchSnapshot();
+    const structure = omitDeep(dir, 'size');
+    const cssDirIndex = _.findIndex(
+      structure.children,
+      ({ name }) => name === 'css'
+    );
+    const cssFiles = structure.children[cssDirIndex].children;
+    structure.children[cssDirIndex].children = cssFiles.map(file => ({
+      ...file,
+      name: replaceFileName(file.name),
+      path: replaceFileName(file.path),
+    }));
+    expect(structure).toMatchSnapshot();
   });
 
   it('generates the correct files', () => {
@@ -13,8 +29,8 @@ describe('Core', () => {
       const file =
         item.extension === '.json'
           ? require(`../${item.path}`) // eslint-disable-line
-          : fs.readFileSync(item.path, 'utf8');
-      expect(file).toMatchSnapshot(item.path);
+          : replaceFileName(fs.readFileSync(item.path, 'utf8'));
+      expect(file).toMatchSnapshot(replaceFileName(item.path));
     });
   });
 });
