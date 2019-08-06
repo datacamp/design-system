03a2bf34adb24434d796c9bffce5555606fdad3d
Louis Bailey <louis@datacamp.com>
2019-08-06 13:23:59 +0200

    chore: switch to react-testing-library
    This brings the testing in the Tag package in line with the testing used 
    across the rest of the repository.

--- packages/react-components/tag/jest.config.js
+++ packages/react-components/tag/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  setupFiles: ['<rootDir>/test/setup.js'],
-  snapshotSerializers: ['jest-emotion', 'enzyme-to-json/serializer'],
+  setupFilesAfterEnv: ['@testing-library/react/cleanup-after-each'],
+  snapshotSerializers: ['jest-emotion'],
   transform: { '\\.[jt]sx?$': '<rootDir>/test/customTransformer.js' },
 };
