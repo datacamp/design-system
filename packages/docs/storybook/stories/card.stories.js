f03bae62b0b3bb4f39d20fc6dc8de4c229ae4d35
Louis Bailey <louis@datacamp.com>
2019-08-08 11:05:01 +0200

    feat: add stories for Card

---
+++ packages/docs/storybook/stories/card.stories.js
@@ -0,0 +1,25 @@
+import Card, { elevationMap } from '@datacamp/waffles-card';
+import { Paragraph } from '@datacamp/waffles-text';
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+
+storiesOf('waffles-card').add('elevation levels', () => (
+  <div
+    css={{
+      backgroundColor: '#F0F4F5',
+      height: '100%',
+      paddingTop: '16px',
+      width: '100%',
+    }}
+  >
+    {Object.keys(elevationMap).map(elevation => (
+      <Card
+        key={elevation}
+        css={{ margin: 16, padding: 24, width: 300 }}
+        elevation={elevation}
+      >
+        <Paragraph>This is a paragraph with some padding.</Paragraph>
+      </Card>
+    ))}
+  </div>
+));
