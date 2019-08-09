f01eb4274327611b24dc5a945ff4eff2b2175935
Louis Bailey <louis@datacamp.com>
2019-08-09 10:40:13 +0200

    feat: add CodeBlock component

---
+++ packages/react-components/text/src/sideEffects/injectMonoFontFace.ts
@@ -0,0 +1,31 @@
+import tokens from '@datacamp/waffles-tokens/lib/future-tokens.json';
+import { css, injectGlobal } from 'emotion';
+
+const monoFont = css(
+  {
+    /* latin-ext */
+    '@font-face': {
+      fontFamily: tokens.asset.font.mono.value,
+      fontStyle: 'normal',
+      fontWeight: tokens.fontWeight.regular.value,
+      src:
+        "local('Roboto Mono'), local('RobotoMono-Regular'), url(https://fonts.gstatic.com/s/robotomono/v5/hMqPNLsu_dywMa4C_DEpY6E8kM4xWR1_1bYURRojRGc.woff2) format('woff2'), url(https://fonts.gstatic.com/s/robotomono/v5/hMqPNLsu_dywMa4C_DEpY4bN6UDyHWBl620a-IRfuBk.woff) format('woff')",
+      unicodeRange:
+        'U+0100-024F, U+0259, U+1E00-1EFF, U+20A0-20AB, U+20AD-20CF, U+2C60-2C7F, U+A720-A7FF',
+    },
+  },
+  {
+    /* latin */
+    '@font-face': {
+      fontFamily: tokens.asset.font.mono.value,
+      fontStyle: 'normal',
+      fontWeight: tokens.fontWeight.regular.value,
+      src:
+        "local('Roboto Mono'), local('RobotoMono-Regular'), url(https://fonts.gstatic.com/s/robotomono/v5/hMqPNLsu_dywMa4C_DEpY4gp9Q8gbYrhqGlRav_IXfk.woff2) format('woff2'), url(https://fonts.gstatic.com/s/robotomono/v5/hMqPNLsu_dywMa4C_DEpY4bN6UDyHWBl620a-IRfuBk.woff) format('woff')",
+      unicodeRange:
+        'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2212, U+2215',
+    },
+  }
+);
+
+injectGlobal(monoFont);
