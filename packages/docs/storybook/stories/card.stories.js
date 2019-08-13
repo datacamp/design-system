27aeef498545e06a071ed7e16b45350e6066a4cd
Louis Bailey <louis@datacamp.com>
2019-08-13 13:08:51 +0200

    feat: control hover state of Card component

--- packages/docs/storybook/stories/card.stories.js
+++ packages/docs/storybook/stories/card.stories.js
@@ -1,26 +1,32 @@
 import Card, { elevationMap } from '@datacamp/waffles-card';
 import { Paragraph } from '@datacamp/waffles-text';
+import { select } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 
-storiesOf('waffles-card').add('elevation levels', () => (
-  <div
-    css={{
-      backgroundColor: '#F0F4F5',
-      height: '100%',
-      paddingTop: '16px',
-      width: '100%',
-    }}
-  >
-    {Object.keys(elevationMap).map(elevation => (
-      <Card
-        key={elevation}
-        css={{ margin: 16, padding: 24, width: 300 }}
-        elevation={elevation}
-        hoverElevation={4}
-      >
-        <Paragraph>This is a paragraph with some padding.</Paragraph>
-      </Card>
-    ))}
-  </div>
-));
+storiesOf('waffles-card', module).add('elevation levels', () => {
+  const hoverElevation = select('hoverElevation', [0, 1, 2, 3, 4]);
+  return (
+    <div
+      css={{
+        backgroundColor: '#F0F4F5',
+        height: '100%',
+        paddingTop: '16px',
+        width: '100%',
+      }}
+    >
+      {Object.keys(elevationMap).map(elevation => (
+        <Card
+          key={elevation}
+          css={{ margin: 16, padding: 24, width: 300 }}
+          elevation={parseInt(elevation, 10)}
+          hoverElevation={
+            hoverElevation > elevation ? hoverElevation : undefined
+          }
+        >
+          <Paragraph>This is a paragraph with some padding.</Paragraph>
+        </Card>
+      ))}
+    </div>
+  );
+});
