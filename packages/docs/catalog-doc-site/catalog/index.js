63305f8fac5523b2dc14715e60684e557cc94be7
Louis Bailey <louis@datacamp.com>
2019-08-29 10:27:49 +0200

    feat: add documentation for alert dialog

--- packages/docs/catalog-doc-site/catalog/index.js
+++ packages/docs/catalog-doc-site/catalog/index.js
@@ -1,11 +1,14 @@
 import '../src/stylesheets/catalog.scss';
 
+import { setAppElement } from '@datacamp/waffles-modals';
 import { Catalog } from 'catalog';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import pages from './config/pages';
 import { logoSrc, responsiveSizes, theme } from './config/theme';
+
+setAppElement('#catalog');
 
 const styles = [];
 const scripts = [];
---
+++ packages/docs/catalog-doc-site/catalog/pages/componentLibrary/modals.js
@@ -0,0 +1,174 @@
+import Button from '@datacamp/waffles-button';
+import { AlertDialog } from '@datacamp/waffles-modals';
+import { Code, CodeBlock, Heading, Paragraph } from '@datacamp/waffles-text';
+import { Page } from 'catalog';
+import React from 'react';
+
+import CustomHeader from '../../components/CustomHeader';
+import LazyLiveEditCells from '../../components/LazyLiveEditCells';
+
+export default () => {
+  return (
+    <main>
+      <CustomHeader
+        description="The modals package"
+        section="Component Library"
+        subSection="Modals"
+      />
+
+      <Page>
+        <section className="dc-u-maxw-100pc">
+          <div className="dc-card dc-u-p-24 dc-u-mt-8">
+            <Heading as="h3" size={500}>
+              About
+            </Heading>
+            <Paragraph>
+              The <Code>@datacamp/waffles-modals</Code> package contains
+              multiple modals that can be used for different purposes. Before
+              using any of the exposed modals, <Code>setAppElement</Code> should
+              be called with the root element of the application. This will
+              normally be the same as the element used for rendering react.
+            </Paragraph>
+            <CodeBlock>
+              {
+                "import { setAppElement } from '@datacamp/waffles-modals';\n\nsetAppElement('#root');"
+              }
+            </CodeBlock>
+          </div>
+          <div className="dc-card dc-u-p-24 dc-u-mt-8">
+            <Heading as="h3" size={500}>
+              Alert Dialog
+            </Heading>
+            <Paragraph>
+              The alert dialog should be used whenever user confirmation is
+              required before performing an action. It takes no children as its
+              behaviour is controlled entirely by the properties set on the
+              component. Below there is a simple example of how this can be
+              used, feel free to change some of the values and see how it
+              impacts the Modal.
+            </Paragraph>
+            <table className="dc-table dc-table--bordered">
+              <tbody>
+                <tr>
+                  <LazyLiveEditCells
+                    code={`() => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setIsOpen(true)}>open modal</Button>
+      <AlertDialog
+        confirmButtonText="Delete"
+        cancelButtonText="Cancel"
+        intent="danger"
+        isOpen={isOpen}
+        title="You are about to delete an exercise"
+        description="Are you sure you want to proceed with this action, this can not be undone."
+        onClose={(origin) => setIsOpen(false)}
+        onConfirm={() => setIsOpen(false)}
+      />
+    </>
+  );
+};`}
+                    scope={{ AlertDialog, Button }}
+                  />
+                </tr>
+              </tbody>
+            </table>
+            <table className="dc-table dc-table--bordered">
+              <thead className="dc-table__thead">
+                <tr className="dc-table__tr">
+                  <th>Property</th>
+                  <th>Type</th>
+                  <th>Description</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="dc-table__tr">
+                  <th>cancelButtonText</th>
+                  <td>string - optional</td>
+                  <td>
+                    By default the cancel button will say &quot;Cancel&quot;.
+                    This can be overriden by this prop.
+                  </td>
+                </tr>
+                <tr className="dc-table__tr">
+                  <th>confirmButtonText</th>
+                  <td>string - optional</td>
+                  <td>
+                    By default the confirm button will say &quot;Confirm&quot;.
+                    This can be overriden by this prop.
+                  </td>
+                </tr>
+                <tr className="dc-table__tr">
+                  <th>dataAttributes</th>
+                  <td>object - optional</td>
+                  <td>
+                    As with all the other waffles components, dataAttributes can
+                    be used to set data- html attributes on the element
+                  </td>
+                </tr>
+                <tr className="dc-table__tr">
+                  <th>description</th>
+                  <td>string - required</td>
+                  <td>The body text to render in the modal</td>
+                </tr>
+                <tr className="dc-table__tr">
+                  <th>intent</th>
+                  <td>neutral | success | warning | danger - optional</td>
+                  <td>
+                    The main intention of the modal. This is used to set the
+                    button colours and to provide a default focus when the modal
+                    opens.
+                  </td>
+                </tr>
+                <tr className="dc-table__tr">
+                  <th>isOpen</th>
+                  <td>boolean - required</td>
+                  <td>
+                    When this is set to true the Modal will show. It is
+                    preferred to use this to control when to display the dialog
+                    rather than simply mounting and unmounting as setting this
+                    to false allows the close animation to play fully.
+                  </td>
+                </tr>
+                <tr className="dc-table__tr">
+                  <th>onClose</th>
+                  <td>
+                    <Code>
+                      (origin: cancelButton | overlayClick | escKey |
+                      closeButton) =&gt; void
+                    </Code>{' '}
+                    - required
+                  </td>
+                  <td>
+                    This function is called whenever the user requests to close
+                    the dialog. It is the responsibility of the application to
+                    handle this correctly and set isOpen to false. This function
+                    will be called with an origin string that specifies how the
+                    user requested to close the modal.
+                  </td>
+                </tr>
+                <tr className="dc-table__tr">
+                  <th>onConfirm</th>
+                  <td>
+                    <Code>() =&gt; void</Code> - required
+                  </td>
+                  <td>
+                    This function is called whenever the user clicks the confirm
+                    button.
+                  </td>
+                </tr>
+                <tr className="dc-table__tr">
+                  <th>title</th>
+                  <td>string - required</td>
+                  <td>The main heading for the modal.</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </Page>
+    </main>
+  );
+};
