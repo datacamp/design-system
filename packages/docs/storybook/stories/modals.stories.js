f037b9da5a4612e9987e09ead8a00fbda0b0636f
Louis Bailey <louis@datacamp.com>
2019-08-29 10:27:34 +0200

    feat: add stories for alert dialog

---
+++ packages/docs/storybook/stories/modals.stories.js
@@ -0,0 +1,35 @@
+import Button from '@datacamp/waffles-button';
+import { AlertDialog, setAppElement } from '@datacamp/waffles-modals';
+import { Paragraph } from '@datacamp/waffles-text';
+import { storiesOf } from '@storybook/react';
+import React, { createElement, useState } from 'react';
+
+setAppElement('#root');
+
+['neutral', 'danger', 'warning', 'success'].forEach(intent => {
+  storiesOf('waffles-modals', module).add(`${intent} AlertDialog`, () => {
+    return createElement(() => {
+      const [isOpen, setIsOpen] = useState('isOpen', true);
+      return (
+        <>
+          <Paragraph>this is some content behind the modal</Paragraph>
+          <Paragraph>this is some content behind the modal</Paragraph>
+          <Button onClick={() => setIsOpen(true)}>Open Modal</Button>
+          <AlertDialog
+            description="Are you sure you want to proceed with this action, this can not be undone afterward."
+            intent={intent}
+            isOpen={isOpen}
+            onClose={origin => {
+              console.log(origin); // eslint-disable-line no-console
+              setIsOpen(false);
+            }}
+            onConfirm={
+              () => alert('confirmed!!') // eslint-disable-line no-alert
+            }
+            title="You are about to delete an exercise"
+          />
+        </>
+      );
+    });
+  });
+});
