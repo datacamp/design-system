69f87e96d27946c9e7f193d1f6d6a1144091dd43
Louis Bailey <louis@datacamp.com>
2019-08-30 15:01:31 +0200

    fix: use SSR safe version of :first-child from waffles-utils

--- packages/react-components/text/src/ssrSafeFirstChildSelector.ts
+++
@@ -1,6 +0,0 @@
-/**
- * This is a css selector equiilant of ':first-child that is compatible with
- * emotion's ssr rendering. It also disables the related warning about using
- * ':first-child' when doing SSR.
- */
-export default ':first-child:not(style), style:first-child + * /* emotion-disable-server-rendering-unsafe-selector-warning-please-do-not-use-this-the-warning-exists-for-a-reason */';
