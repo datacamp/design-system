5ebcdc3f303d00f8d53e8eb8b43ab17819383e83
cat <caterina.mennito@gmail.com>
2019-08-30 11:26:29 +0200

    feat: add story for type="link"

--- packages/docs/storybook/stories/button.stories.js
+++ packages/docs/storybook/stories/button.stories.js
@@ -8,174 +8,256 @@ storiesOf('waffles-button', module).add('Button Component', () => (
   <table>
     <tr>
       <td>
-        <Button appearance="primary" size="small">
+        <Button appearance="primary" onClick={() => {}} size="small">
           small
         </Button>
       </td>
       <td>
-        <Button appearance="primary" intent="danger" size="small">
+        <Button
+          appearance="primary"
+          intent="danger"
+          onClick={() => {}}
+          size="small"
+        >
           small
         </Button>
       </td>
       <td>
-        <Button appearance="primary" intent="success" size="small">
+        <Button
+          appearance="primary"
+          intent="success"
+          onClick={() => {}}
+          size="small"
+        >
           small
         </Button>
       </td>
       <td>
-        <Button appearance="primary" intent="warning" size="small">
+        <Button
+          appearance="primary"
+          intent="warning"
+          onClick={() => {}}
+          size="small"
+        >
           small
         </Button>
       </td>
     </tr>
     <tr>
       <td>
-        {' '}
-        <Button appearance="primary">Default</Button>
+        <Button appearance="primary" onClick={() => {}}>
+          Default
+        </Button>
       </td>
       <td>
-        {' '}
-        <Button appearance="primary" intent="danger">
+        <Button appearance="primary" intent="danger" onClick={() => {}}>
           Danger
         </Button>
       </td>
       <td>
-        {' '}
-        <Button appearance="primary" className="test" intent="success">
+        <Button
+          appearance="primary"
+          className="test"
+          intent="success"
+          onClick={() => {}}
+        >
           Success
         </Button>
       </td>
       <td>
-        {' '}
-        <Button appearance="primary" intent="warning">
+        <Button appearance="primary" intent="warning" onClick={() => {}}>
           Warning
         </Button>
       </td>
     </tr>
     <tr>
       <td>
-        <Button appearance="primary" size="large">
-          large{' '}
+        <Button appearance="primary" onClick={() => {}} size="large">
+          Large
         </Button>
       </td>
       <td>
-        <Button appearance="primary" intent="danger" size="large">
+        <Button
+          appearance="primary"
+          intent="danger"
+          onClick={() => {}}
+          size="large"
+        >
           large
         </Button>
       </td>
       <td>
-        <Button appearance="primary" intent="success" size="large">
+        <Button
+          appearance="primary"
+          intent="success"
+          onClick={() => {}}
+          size="large"
+        >
           large
         </Button>
       </td>
       <td>
-        <Button appearance="primary" intent="warning" size="large">
+        <Button
+          appearance="primary"
+          intent="warning"
+          onClick={() => {}}
+          size="large"
+        >
           large
         </Button>
       </td>
     </tr>
     <tr>
       <td>
-        <Button appearance="default" size="small">
+        <Button appearance="default" onClick={() => {}} size="small">
           outline small
         </Button>
       </td>
       <td>
-        <Button appearance="default" intent="danger" size="small">
+        <Button
+          appearance="default"
+          intent="danger"
+          onClick={() => {}}
+          size="small"
+        >
           outline danger small
         </Button>
       </td>
       <td>
-        <Button appearance="default" intent="success" size="small">
+        <Button
+          appearance="default"
+          intent="success"
+          onClick={() => {}}
+          size="small"
+        >
           outline success small
         </Button>
       </td>
       <td>
-        <Button appearance="default" intent="warning" size="small">
+        <Button
+          appearance="default"
+          intent="warning"
+          onClick={() => {}}
+          size="small"
+        >
           outline warning small
         </Button>
       </td>
     </tr>
     <tr>
       <td>
-        <Button appearance="default">outline</Button>
+        <Button appearance="default" onClick={() => {}}>
+          outline
+        </Button>
       </td>
       <td>
-        <Button appearance="default" intent="danger">
+        <Button appearance="default" intent="danger" onClick={() => {}}>
           outline danger
         </Button>
       </td>
       <td>
-        <Button appearance="default" intent="success">
+        <Button appearance="default" intent="success" onClick={() => {}}>
           outline success
         </Button>
       </td>
       <td>
-        <Button appearance="default" intent="warning">
+        <Button appearance="default" intent="warning" onClick={() => {}}>
           outline warning
         </Button>
       </td>
     </tr>
     <tr>
       <td>
-        <Button appearance="default" size="large">
+        <Button appearance="default" onClick={() => {}} size="large">
           outline large
         </Button>
       </td>
       <td>
-        <Button appearance="default" intent="danger" size="large">
+        <Button
+          appearance="default"
+          intent="danger"
+          onClick={() => {}}
+          size="large"
+        >
           outline danger large
         </Button>
       </td>
       <td>
-        <Button appearance="default" intent="success" size="large">
+        <Button
+          appearance="default"
+          intent="success"
+          onClick={() => {}}
+          size="large"
+        >
           outline success large
         </Button>
       </td>
       <td>
-        <Button appearance="default" intent="warning" size="large">
+        <Button
+          appearance="default"
+          intent="warning"
+          onClick={() => {}}
+          size="large"
+        >
           outline warning large
         </Button>
       </td>
     </tr>
     <tr>
       <td>
-        <Button disabled>Disabled</Button>
-      </td>
-      <td>
-        <Button intent="danger" disabled>
+        <Button onClick={() => {}} disabled>
           Disabled
         </Button>
       </td>
       <td>
-        <Button intent="success" disabled>
+        <Button intent="danger" onClick={() => {}} disabled>
           Disabled
         </Button>
       </td>
       <td>
-        <Button intent="warning" disabled>
+        <Button intent="success" onClick={() => {}} disabled>
+          Disabled
+        </Button>
+      </td>
+      <td>
+        <Button intent="warning" onClick={() => {}} disabled>
           Disabled
         </Button>
       </td>
     </tr>
     <tr>
       <td>
-        <Button appearance="primary" disabled>
+        <Button appearance="primary" onClick={() => {}} disabled>
           Disabled
         </Button>
       </td>
       <td>
-        <Button appearance="primary" intent="danger" disabled>
+        <Button
+          appearance="primary"
+          intent="danger"
+          onClick={() => {}}
+          disabled
+        >
           Disabled
         </Button>
       </td>
       <td>
-        <Button appearance="primary" intent="success" disabled>
+        <Button
+          appearance="primary"
+          intent="success"
+          onClick={() => {}}
+          disabled
+        >
           Disabled
         </Button>
       </td>
       <td>
-        <Button appearance="primary" intent="warning" disabled>
+        <Button
+          appearance="primary"
+          intent="warning"
+          onClick={() => {}}
+          disabled
+        >
           Disabled
         </Button>
       </td>
@@ -283,6 +365,45 @@ storiesOf('waffles-button', module).add('Button Component', () => (
       <td>
         <Button intent="warning" size="small" isLoading>
           test different sizes
+        </Button>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <Button appearance="primary" href="https://www.google.com/" type="link">
+          Link
+        </Button>
+      </td>
+      <td>
+        <Button
+          appearance="primary"
+          href="https://www.google.com/"
+          intent="danger"
+          target="_blank"
+          type="link"
+        >
+          Link target blank
+        </Button>
+      </td>
+      <td>
+        <Button
+          href="https://www.google.com/"
+          intent="warning"
+          size="small"
+          target="_blank"
+          type="link"
+        >
+          Link small target blank
+        </Button>
+      </td>
+      <td>
+        <Button
+          href="https://www.google.com/?"
+          intent="success"
+          size="large"
+          type="link"
+        >
+          Link Large
         </Button>
       </td>
     </tr>
