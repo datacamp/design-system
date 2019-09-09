6a049d7ede91e3633ba0d770d5d25c8a101c41f7
Louis Bailey <louis@datacamp.com>
2019-09-09 14:03:03 +0200

    feat: stories for input component

---
+++ packages/docs/storybook/stories/form-elements.stories.js
@@ -0,0 +1,51 @@
+import { Input } from '@datacamp/waffles-form-elements';
+import { storiesOf } from '@storybook/react';
+import React, { createElement, useEffect, useRef, useState } from 'react';
+
+storiesOf('waffles-form-elements', module).add('Input', () => {
+  return createElement(() => {
+    const [value, setValue] = useState('');
+    const focusRef = useRef();
+    useEffect(() => {
+      if (focusRef && focusRef.current) {
+        focusRef.current.focus();
+      }
+    }, [focusRef]);
+
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <Input
+                name="story"
+                onChange={() => {}}
+                placeholder="placeholder text"
+                value=""
+              />
+            </td>
+            <td>
+              <Input name="story" onChange={() => {}} value="with a value" />
+            </td>
+            <td>
+              <Input
+                ref={focusRef}
+                name="story"
+                onChange={() => {}}
+                value="initial focus"
+              />
+            </td>
+            <td>
+              <Input
+                name="story"
+                onChange={setValue}
+                placeholder="editable"
+                value={value}
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  });
+});
