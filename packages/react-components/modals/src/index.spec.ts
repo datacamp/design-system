3aa736b1dc9313ded0a4d731a7b429a327a791fe
Louis Bailey <louis@datacamp.com>
2019-09-10 16:47:20 +0200

    feat: add Dialog component

--- packages/react-components/modals/src/index.spec.ts
+++ packages/react-components/modals/src/index.spec.ts
@@ -2,9 +2,10 @@ import { setAppElement } from 'react-modal';
 
 import * as packageExports from '.';
 import AlertDialog from './AlertDialog';
+import Dialog from './Dialog';
 
 describe('package index', () => {
   it('exports the AlertModal & setAppElement', () => {
-    expect(packageExports).toEqual({ AlertDialog, setAppElement });
+    expect(packageExports).toEqual({ AlertDialog, Dialog, setAppElement });
   });
 });
--- packages/react-components/modals/src/index.tsx
+++ packages/react-components/modals/src/index.tsx
@@ -1,5 +1,6 @@
 import ReactModal from 'react-modal';
 
 export { default as AlertDialog } from './AlertDialog';
+export { default as Dialog } from './Dialog';
 
 export const { setAppElement } = ReactModal;
