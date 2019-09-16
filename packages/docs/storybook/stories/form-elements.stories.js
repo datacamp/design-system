fd2d5582b11fa1ac4ba69b3f2e3586db6ce89a03
cat <caterina.mennito@gmail.com>
2019-09-16 11:35:53 +0200

    feat: add story for input with label

--- packages/docs/storybook/stories/form-elements.stories.js
+++ packages/docs/storybook/stories/form-elements.stories.js
@@ -2,59 +2,103 @@ import { Input } from '@datacamp/waffles-form-elements';
 import { storiesOf } from '@storybook/react';
 import React, { createElement, useEffect, useRef, useState } from 'react';
 
-storiesOf('waffles-form-elements', module).add('Input', () => {
-  return createElement(() => {
-    const [value, setValue] = useState('');
-    const focusRef = useRef();
-    useEffect(() => {
-      if (focusRef && focusRef.current) {
-        focusRef.current.focus();
-      }
-    }, [focusRef]);
+storiesOf('waffles-form-elements', module)
+  .add('Input', () => {
+    return createElement(() => {
+      const [value, setValue] = useState('');
+      const focusRef = useRef();
+      useEffect(() => {
+        if (focusRef && focusRef.current) {
+          focusRef.current.focus();
+        }
+      }, [focusRef]);
 
-    return (
-      <table>
-        <tbody>
-          <tr>
-            <td>
-              <Input
-                name="story"
-                onChange={() => {}}
-                placeholder="placeholder text"
-                value=""
-              />
-            </td>
-            <td>
-              <Input name="story" onChange={() => {}} value="with a value" />
-            </td>
-            <td>
-              <Input
-                ref={focusRef}
-                name="story"
-                onChange={() => {}}
-                value="initial focus"
-              />
-            </td>
-            <td>
-              <Input
-                name="story"
-                onChange={setValue}
-                placeholder="editable"
-                value={value}
-              />
-            </td>
-            <td>
-              <Input
-                name="story"
-                onChange={setValue}
-                placeholder="disabled"
-                value={value}
-                disabled
-              />
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    );
+      return (
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <Input
+                  name="story"
+                  onChange={() => {}}
+                  placeholder="placeholder text"
+                  value=""
+                />
+              </td>
+              <td>
+                <Input name="story" onChange={() => {}} value="with a value" />
+              </td>
+              <td>
+                <Input
+                  ref={focusRef}
+                  name="story"
+                  onChange={() => {}}
+                  value="initial focus"
+                />
+              </td>
+              <td>
+                <Input
+                  name="story"
+                  onChange={setValue}
+                  placeholder="editable"
+                  value={value}
+                />
+              </td>
+              <td>
+                <Input
+                  name="story"
+                  onChange={() => {}}
+                  placeholder="disabled"
+                  value=""
+                  disabled
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      );
+    });
+  })
+  .add('Input with label', () => {
+    return createElement(() => {
+      const focusRef = useRef();
+      useEffect(() => {
+        if (focusRef && focusRef.current) {
+          focusRef.current.focus();
+        }
+      }, [focusRef]);
+
+      return (
+        <div>
+          <Input
+            label="Test label"
+            name="story1"
+            onChange={() => {}}
+            placeholder="with label"
+            value=""
+          />
+          <Input
+            label="Another test label"
+            name="story2"
+            onChange={() => {}}
+            placeholder="with label"
+            value=""
+          />
+          <Input
+            label="Another test label"
+            name="story3"
+            onChange={() => {}}
+            placeholder="with label"
+            value=""
+          />
+          <Input
+            label="Another test label"
+            name="story4"
+            onChange={() => {}}
+            placeholder="with label"
+            value=""
+          />
+        </div>
+      );
+    });
   });
-});
