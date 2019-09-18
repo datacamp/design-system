d4bc868f674e029e81b7b2f4320beecab6141f35
cat <caterina.mennito@gmail.com>
2019-09-18 17:15:04 +0200

    fix: lint (+3 squashed commits) Squashed commits: [210254c] feat: add doc [b7486d8] feat: use button group in footer [f91c7ae] feat: button group in footer

--- packages/docs/storybook/stories/modals.stories.js
+++ packages/docs/storybook/stories/modals.stories.js
@@ -1,4 +1,4 @@
-import Button from '@datacamp/waffles-button';
+import Button, { ButtonGroup } from '@datacamp/waffles-button';
 import { AlertDialog, Dialog, setAppElement } from '@datacamp/waffles-modals';
 import { Heading, Paragraph } from '@datacamp/waffles-text';
 import { storiesOf } from '@storybook/react';
@@ -159,7 +159,7 @@ storiesOf('waffles-modals', module)
       );
     });
   })
-  .add('Dialog with Header, Body & Footer with two buttons', () => {
+  .add('Dialog with Header, Body & Footer with ButtonGroup', () => {
     return createElement(() => {
       const [isOpen, setIsOpen] = useState(true);
       return (
@@ -189,6 +189,49 @@ storiesOf('waffles-modals', module)
             </Dialog.Body>
             <Dialog.Footer>
               <Button>Button</Button>
+              <Button appearance="primary" type="submit">
+                Button
+              </Button>
+            </Dialog.Footer>
+          </Dialog>
+        </>
+      );
+    });
+  })
+
+  .add('Dialog with Header, Body & Footer with ButtonGroup', () => {
+    return createElement(() => {
+      const [isOpen, setIsOpen] = useState(true);
+      return (
+        <>
+          <Paragraph>this is some content behind the modal</Paragraph>
+          <Paragraph>this is some content behind the modal</Paragraph>
+          <Button onClick={() => setIsOpen(true)}>Open Modal</Button>
+          <Dialog
+            isOpen={isOpen}
+            onClose={origin => {
+              console.log(origin); // eslint-disable-line no-console
+              setIsOpen(false);
+            }}
+            width="600px"
+          >
+            <Dialog.Header>Dialog with a Body and Footer</Dialog.Header>
+            <Dialog.Body>
+              <Paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum eu sapien imperdiet, cursus ex at, hendrerit nisl.
+                Nulla non gravida libero. Duis quis lacus odio. Suspendisse
+                neque turpis, finibus id est non, vehicula vestibulum sapien.
+                Mauris pharetra mi eu est aliquet posuere. Aenean ullamcorper,
+                mauris euismod molestie suscipit, dolor nisl rutrum quam, eu
+                eleifend elit ipsum ac nisl.
+              </Paragraph>
+            </Dialog.Body>
+            <Dialog.Footer>
+              <ButtonGroup>
+                <Button>ButtonGroup</Button>
+                <Button>ButtonGroup</Button>
+              </ButtonGroup>
               <Button appearance="primary" type="submit">
                 Button
               </Button>
