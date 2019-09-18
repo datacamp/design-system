05a972ff754904415187307e1669f571b43134c8
cat <caterina.mennito@gmail.com>
2019-09-18 11:38:05 +0200

    feat: add story for input size

--- packages/docs/storybook/stories/form-elements.stories.js
+++ packages/docs/storybook/stories/form-elements.stories.js
@@ -1,3 +1,4 @@
+import Button from '@datacamp/waffles-button';
 import { Input } from '@datacamp/waffles-form-elements';
 import { storiesOf } from '@storybook/react';
 import React, { createElement, useEffect, useRef, useState } from 'react';
@@ -14,48 +15,83 @@ storiesOf('waffles-form-elements', module)
       }, [focusRef]);
 
       return (
-        <table>
-          <tbody>
-            <tr>
-              <td>
-                <Input
-                  name="story"
-                  onChange={() => {}}
-                  placeholder="placeholder text"
-                  value=""
-                />
-              </td>
-              <td>
-                <Input name="story" onChange={() => {}} value="with a value" />
-              </td>
-              <td>
-                <Input
-                  ref={focusRef}
-                  name="story"
-                  onChange={() => {}}
-                  value="initial focus"
-                />
-              </td>
-              <td>
-                <Input
-                  name="story"
-                  onChange={setValue}
-                  placeholder="editable"
-                  value={value}
-                />
-              </td>
-              <td>
-                <Input
-                  name="story"
-                  onChange={() => {}}
-                  placeholder="disabled"
-                  value=""
-                  disabled
-                />
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <Input
+                    name="story"
+                    onChange={() => {}}
+                    placeholder="placeholder text"
+                    value=""
+                  />
+                </td>
+                <td>
+                  <Input
+                    name="story"
+                    onChange={() => {}}
+                    value="with a value"
+                  />
+                </td>
+                <td>
+                  <Input
+                    ref={focusRef}
+                    name="story"
+                    onChange={() => {}}
+                    value="initial focus"
+                  />
+                </td>
+                <td>
+                  <Input
+                    name="story"
+                    onChange={setValue}
+                    placeholder="editable"
+                    value={value}
+                  />
+                </td>
+                <td>
+                  <Input
+                    name="story"
+                    onChange={() => {}}
+                    placeholder="disabled"
+                    value=""
+                    disabled
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div>
+            <Button size="small">Small</Button>
+            <Input
+              name="story5"
+              onChange={() => {}}
+              placeholder="small input"
+              size="small"
+              value=""
+            />
+          </div>
+          <div>
+            <Button>medium</Button>
+            <Input
+              name="story5"
+              onChange={() => {}}
+              placeholder="medium input"
+              value=""
+            />
+          </div>
+          <div>
+            <Button size="large">large</Button>
+            <Input
+              name="story5"
+              onChange={() => {}}
+              placeholder="large input"
+              size="large"
+              value=""
+            />
+          </div>
+        </div>
       );
     });
   })
