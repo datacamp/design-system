f98c2988e731b30a4c8bb2bfc886b32374aba8c0
cat <caterina.mennito@gmail.com>
2019-09-19 13:53:55 +0200

    feat: allow two buttons in the footer

--- packages/react-components/modals/src/Dialog/Footer.tsx
+++ packages/react-components/modals/src/Dialog/Footer.tsx
@@ -1,27 +1,42 @@
 import Button from '@datacamp/waffles-button';
 import tokens from '@datacamp/waffles-tokens/lib/future-tokens.json';
-import { childrenOfType } from 'airbnb-prop-types';
+import { css } from '@emotion/core';
+import { childrenOfType, nChildren } from 'airbnb-prop-types';
+import PropTypes from 'prop-types';
 import React from 'react';
 
 export interface FooterProps {
-  children: React.ReactElement;
+  children: React.ReactElement | React.ReactElement[];
 }
+
+const baseStyle = css({
+  boxShadow: '0px -1px 0px 0px rgb(230, 234, 235)',
+  display: 'flex',
+  padding: tokens.size.space[16].value,
+});
+
+const twoChildrenStyle = css(baseStyle, {
+  justifyContent: 'space-between',
+});
+
+const defaultStyle = css(baseStyle, {
+  justifyContent: 'space-around',
+  textAlign: 'center',
+});
 
 const Footer = ({ children }: FooterProps): React.ReactElement => (
   <div
-    css={{
-      boxShadow: '0px -1px 0px 0px rgb(230, 234, 235)',
-      paddingBottom: tokens.size.space[16].value,
-      paddingTop: tokens.size.space[16].value,
-      textAlign: 'center',
-    }}
+    css={React.Children.count(children) === 2 ? twoChildrenStyle : defaultStyle}
   >
     {children}
   </div>
 );
 
 Footer.propTypes = {
-  children: childrenOfType(Button).isRequired,
+  children: PropTypes.oneOfType([
+    nChildren(2, PropTypes.arrayOf(childrenOfType(Button))),
+    childrenOfType(Button).isRequired,
+  ]),
 };
 
 export default Footer;
