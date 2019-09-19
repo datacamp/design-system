b29b4bfed76186cc4d1558b4be55850fdf47796b
cat <caterina.mennito@gmail.com>
2019-09-19 13:54:29 +0200

    feat: story for two buttons in the footer

--- packages/docs/storybook/stories/modals.stories.js
+++ packages/docs/storybook/stories/modals.stories.js
@@ -158,4 +158,43 @@ storiesOf('waffles-modals', module)
         </>
       );
     });
+  })
+  .add('Dialog with Header, Body & Footer with two buttons', () => {
+    return createElement(() => {
+      const [isOpen, setIsOpen] = useState(true);
+      return (
+        <>
+          <Paragraph>this is some content behind the modal</Paragraph>
+          <Paragraph>this is some content behind the modal</Paragraph>
+          <Button onClick={() => setIsOpen(true)}>Open Modal</Button>
+          <Dialog
+            isOpen={isOpen}
+            onClose={origin => {
+              console.log(origin); // eslint-disable-line no-console
+              setIsOpen(false);
+            }}
+            width="600px"
+          >
+            <Dialog.Header>Dialog with a Body and Footer</Dialog.Header>
+            <Dialog.Body>
+              <Paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum eu sapien imperdiet, cursus ex at, hendrerit nisl.
+                Nulla non gravida libero. Duis quis lacus odio. Suspendisse
+                neque turpis, finibus id est non, vehicula vestibulum sapien.
+                Mauris pharetra mi eu est aliquet posuere. Aenean ullamcorper,
+                mauris euismod molestie suscipit, dolor nisl rutrum quam, eu
+                eleifend elit ipsum ac nisl.
+              </Paragraph>
+            </Dialog.Body>
+            <Dialog.Footer>
+              <Button>Button</Button>
+              <Button appearance="primary" type="submit">
+                Button
+              </Button>
+            </Dialog.Footer>
+          </Dialog>
+        </>
+      );
+    });
   });
