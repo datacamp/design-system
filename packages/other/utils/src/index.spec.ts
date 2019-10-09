40414b2405fd2c36e961e26a9e052f6d938cf620
Louis Bailey <louis@datacamp.com>
2019-10-09 11:33:22 +0200

    test: add test for new util

--- packages/other/utils/src/index.spec.ts
+++ packages/other/utils/src/index.spec.ts
@@ -1,9 +1,14 @@
 import * as utils from '.';
 import computeDataAttributes from './computeDataAttributes';
+import isChildType from './isChildType';
 import ssrSafeFirstChildSelector from './ssrSafeFirstChildSelector';
 
 describe('waffles-utils', () => {
   it('exposes all the utils', () => {
-    expect(utils).toEqual({ computeDataAttributes, ssrSafeFirstChildSelector });
+    expect(utils).toEqual({
+      computeDataAttributes,
+      isChildType,
+      ssrSafeFirstChildSelector,
+    });
   });
 });
