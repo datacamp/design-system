4575219d5ce42b28937ad3ffeffd5750f31caf91
Louis Bailey <louis@datacamp.com>
2019-10-15 15:39:49 +0200

    fix: combine all icons into one story

--- packages/docs/storybook/stories/icons.stories.js
+++ packages/docs/storybook/stories/icons.stories.js
@@ -1,14 +1,30 @@
 import * as Icons from '@datacamp/waffles-icons';
+import { Text } from '@datacamp/waffles-text';
 import { color } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 
-Object.entries(Icons).forEach(([name, Component]) => {
-  storiesOf('waffles-icons', module).add(name, () => (
-    <>
-      <Component color={color('color', 'currentColor')} size={12} />{' '}
-      <Component color={color('color', 'currentColor')} size={18} />{' '}
-      <Component color={color('color', 'currentColor')} size={24} />
-    </>
+storiesOf('waffles-icons', module)
+  .addParameters({ percy: { widths: [500] } })
+  .add('icons', () => (
+    <table css={{ width: '100%' }}>
+      <tbody>
+        {Object.entries(Icons).map(([name, Component]) => (
+          <tr key={name}>
+            <td>
+              <Text>{name}</Text>
+            </td>
+            <td>
+              <Component color={color('color', 'currentColor')} size={12} />
+            </td>
+            <td>
+              <Component color={color('color', 'currentColor')} size={18} />
+            </td>
+            <td>
+              <Component color={color('color', 'currentColor')} size={24} />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   ));
-});
