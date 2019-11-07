a7de3b2cc92d8485871f776b5bf790c09955334a
Louis Bailey <louis@datacamp.com>
2019-11-07 15:01:00 +0100

    fix: antialias fonts

--- packages/react-components/text/src/baseStyle.ts
+++ packages/react-components/text/src/baseStyle.ts
@@ -4,7 +4,7 @@ import './sideEffects/injectRegularFontFace';
 import tokens from '@datacamp/waffles-tokens/lib/future-tokens.json';
 import { css } from '@emotion/core';
 
-export default css({
+export default css('-webkit-font-smoothing: antialiased;', {
   color: tokens.color.opaque.grey.value.rgb,
   fontFamily: [
     tokens.asset.font.sansSerif.attributes.fallback,
