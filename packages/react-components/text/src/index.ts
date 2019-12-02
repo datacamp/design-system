3a7834b5bba9072f757d4fe0c53aac20853db2ea
Louis Bailey <louis@datacamp.com>
2019-12-02 14:44:03 +0100

    feat: add BetaBadge component

--- packages/react-components/text/src/index.ts
+++ packages/react-components/text/src/index.ts
@@ -14,6 +14,7 @@ export { default as Code } from './components/Code';
 export { default as Link } from './components/Link';
 export { default as GlobalFontFaces } from './components/GlobalFontFaces';
 export { default as Badge } from './components/Badge';
+export { default as BetaBadge } from './components/BetaBadge';
 
 export const alternateComponents = {
   AlternateCode,
