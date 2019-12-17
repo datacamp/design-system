c0678394f3e644288b0fd06b54cd12293990f352
Louis Bailey <louis@datacamp.com>
2019-12-17 13:27:05 +0100

    feat: update testing library

--- packages/react-components/tag/jest.config.js
+++ packages/react-components/tag/jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  setupFilesAfterEnv: ['@testing-library/react/cleanup-after-each'],
   snapshotSerializers: ['jest-emotion'],
   transform: { '\\.[jt]sx?$': '<rootDir>/test/customTransformer.js' },
 };
