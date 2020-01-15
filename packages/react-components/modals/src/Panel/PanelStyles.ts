8ae4dac4fb7e852b1583e6694e8c1e89f9fa3a10
Louis Bailey <louis@datacamp.com>
2020-01-15 13:53:09 +0100

    feat: add position prop to Panel to place on left or right of screen

--- packages/react-components/modals/src/Panel/PanelStyles.ts
+++ packages/react-components/modals/src/Panel/PanelStyles.ts
@@ -1,25 +1,39 @@
 import { css } from '@emotion/core';
 
 export const animationTime = 600;
-
-export const contentStyle = css({
+const baseContentStyle = css({
   bottom: '0',
   margin: 'auto',
   opacity: 0,
   outline: 'none',
   position: 'absolute',
-  right: '0',
   top: '0',
-  transform: 'translate(100%, 0)',
   transition: `0.4s cubic-bezier(0.19, 1, 0.22, 1);`,
 });
+
+export const contentStyles = {
+  left: css(baseContentStyle, {
+    left: '0',
+    transform: 'translate(-100%, 0)',
+  }),
+  right: css(baseContentStyle, {
+    right: '0',
+    transform: 'translate(100%, 0)',
+  }),
+};
 
 export const contentStyleAfterOpen = css`
   opacity: 1 !important;
   transform: translate(0, 0) !important;
 `;
 
-export const contentStyleBeforeClose = css`
-  opacity: 0 !important;
-  transform: translate(100%, 0) !important;
-`;
+export const contentStylesBeforeClose = {
+  left: css`
+    opacity: 0 !important;
+    transform: translate(-100%, 0) !important;
+  `,
+  right: css`
+    opacity: 0 !important;
+    transform: translate(100%, 0) !important;
+  `,
+};
