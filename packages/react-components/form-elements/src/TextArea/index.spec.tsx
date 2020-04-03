b931d76e6558fc969e04ea4378cdb5a40bbd5266
Louis Bailey <louis@datacamp.com>
2020-04-03 11:54:32 +0200

    feat: add TextArea component

---
+++ packages/react-components/form-elements/src/TextArea/index.spec.tsx
@@ -0,0 +1,305 @@
+import '@testing-library/jest-dom/extend-expect';
+
+import tokens from '@datacamp/waffles-tokens/lib/future-tokens.json';
+import { fireEvent, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+
+import TextArea from '.';
+
+describe('<TextArea />', () => {
+  describe('without label', () => {
+    // cannot use axeRender here as no label will fail
+
+    const testName = 'test-name';
+    const testValue = 'test value';
+
+    it('sets the correct name and value on the input', () => {
+      const { getByRole } = render(
+        <TextArea name={testName} onChange={() => {}} value={testValue} />,
+      );
+
+      const inputElement = getByRole('textbox') as HTMLElement;
+
+      expect(inputElement).toHaveAttribute('name', testName);
+      expect(inputElement).toHaveTextContent(testValue);
+    });
+
+    it('sets the autocomplete attribute', () => {
+      const testAutocomplete = 'off';
+      const { getByRole } = render(
+        <TextArea
+          autocomplete={testAutocomplete}
+          name={testName}
+          onChange={() => {}}
+          value={testValue}
+        />,
+      );
+
+      const inputElement = getByRole('textbox') as HTMLElement;
+
+      expect(inputElement).toHaveAttribute('autocomplete', testAutocomplete);
+    });
+
+    it('sets the className ', () => {
+      const testClassName = 'test-class';
+      const { getByRole } = render(
+        <TextArea
+          className={testClassName}
+          name={testName}
+          onChange={() => {}}
+          value={testValue}
+        />,
+      );
+      const inputElement = getByRole('textbox') as HTMLElement;
+
+      expect(inputElement).toHaveClass(testClassName);
+    });
+
+    it('sets the data attributes', () => {
+      const testDataAttributes = { test: 'example' };
+      const { getByRole } = render(
+        <TextArea
+          dataAttributes={testDataAttributes}
+          name={testName}
+          onChange={() => {}}
+          value={testValue}
+        />,
+      );
+      const inputElement = getByRole('textbox') as HTMLElement;
+
+      expect(inputElement).toHaveAttribute('data-test', 'example');
+    });
+
+    it('sets the maxLength', () => {
+      const maxLength = 5;
+      const { getByRole } = render(
+        <TextArea
+          maxLength={maxLength}
+          name={testName}
+          onChange={() => {}}
+          value={testValue}
+        />,
+      );
+
+      const inputElement = getByRole('textbox') as HTMLElement;
+
+      expect(inputElement).toHaveAttribute('maxLength', maxLength.toString());
+    });
+
+    it('calls the onBlur function', () => {
+      const onBlur = jest.fn();
+      const { getByRole } = render(
+        <TextArea
+          name={testName}
+          onBlur={onBlur}
+          onChange={() => {}}
+          value={testValue}
+        />,
+      );
+
+      const inputElement = getByRole('textbox') as HTMLElement;
+
+      fireEvent.blur(inputElement);
+      expect(onBlur).toHaveBeenCalledTimes(1);
+    });
+
+    it('calls the onChange function', () => {
+      const onChange = jest.fn();
+      const { getByRole } = render(
+        <TextArea name={testName} onChange={onChange} value={testValue} />,
+      );
+      const testString = 'test string';
+      const inputElement = getByRole('textbox') as HTMLElement;
+
+      userEvent.type(inputElement, testString);
+      expect(onChange).toHaveBeenCalledWith(testString);
+    });
+
+    it('sets the placeholder', () => {
+      const placeholder = 'placeholder text';
+      const { getByRole } = render(
+        <TextArea
+          name={testName}
+          onChange={() => {}}
+          placeholder={placeholder}
+          value={testValue}
+        />,
+      );
+      expect(getByRole('textbox') as HTMLElement).toHaveAttribute(
+        'placeholder',
+        placeholder,
+      );
+    });
+
+    it('disables the input if disabled is passed as a prop', () => {
+      const { getByRole } = render(
+        <TextArea
+          disabled
+          name={testName}
+          onChange={() => {}}
+          value={testValue}
+        />,
+      );
+      expect(getByRole('textbox')).toHaveAttribute('disabled');
+    });
+
+    it('sets the id', () => {
+      const testId = 'name input';
+      const { getByRole } = render(
+        <TextArea
+          id={testId}
+          name={testName}
+          onChange={() => {}}
+          value={testValue}
+        />,
+      );
+      expect(getByRole('textbox')).toHaveAttribute('id', testId);
+    });
+
+    it('disables the input if "disabled" is passed as a prop', () => {
+      const placeholder = 'placeholder text';
+      const { getByRole } = render(
+        <TextArea
+          disabled
+          name={testName}
+          onChange={() => {}}
+          placeholder={placeholder}
+          value={testValue}
+        />,
+      );
+      expect(getByRole('textbox')).toHaveAttribute('disabled');
+    });
+
+    it('sets the rows attribute', () => {
+      const placeholder = 'placeholder text';
+      const { getByRole } = render(
+        <TextArea
+          name={testName}
+          onChange={() => {}}
+          placeholder={placeholder}
+          rows={4}
+          value={testValue}
+        />,
+      );
+      expect(getByRole('textbox')).toHaveAttribute('rows', '4');
+    });
+
+    it('adds an error message under the input field if error is passed as a prop', () => {
+      const testLabel = 'label text';
+      const testError = 'Test error';
+
+      const { getByText } = render(
+        <TextArea
+          errorMessage={testError}
+          label={testLabel}
+          name={testName}
+          onChange={() => {}}
+          value={testValue}
+        />,
+      );
+      expect(getByText(testError)).toBeInTheDocument();
+      expect(getByText(testError)).toHaveStyle(
+        `color: ${tokens.color.opaque.red.value.hex}`,
+      );
+      expect(getByText(testError)).toHaveStyle(`font-size: 14px`);
+    });
+
+    it('renders the indication "required" above the input field if required is passed as a prop ', () => {
+      const testLabel = 'label text';
+
+      const { getByText } = render(
+        <TextArea
+          label={testLabel}
+          name={testName}
+          onChange={() => {}}
+          required
+          value={testValue}
+        />,
+      );
+      expect(getByText('Required')).toBeInTheDocument();
+      expect(getByText('Required')).toHaveStyle(
+        `color: ${tokens.color.opaque.greyOslo.value.hex};`,
+      );
+      expect(getByText('Required')).toHaveStyle(`font-size: 14px;`);
+    });
+
+    it('renders the indication "Optional" above the input field if required={false} is passed as a prop ', () => {
+      const testLabel = 'label text';
+
+      const { getByText } = render(
+        <TextArea
+          label={testLabel}
+          name={testName}
+          onChange={() => {}}
+          required={false}
+          value={testValue}
+        />,
+      );
+      expect(getByText('Optional')).toBeInTheDocument();
+      expect(getByText('Optional')).toHaveStyle(
+        `color: ${tokens.color.opaque.greyOslo.value.hex};`,
+      );
+      expect(getByText('Optional')).toHaveStyle(`font-size: 14px;`);
+    });
+  });
+});
+
+describe('with label', () => {
+  const testName = 'test name';
+  const testValue = 'test value';
+  it('sets the label', () => {
+    const testLabel = 'label text';
+    const { getByLabelText } = render(
+      <TextArea
+        label={testLabel}
+        name={testName}
+        onChange={() => {}}
+        value={testValue}
+      />,
+    );
+    expect(getByLabelText(testLabel)).toBeInTheDocument();
+  });
+
+  it('sets the description', () => {
+    const testLabel = 'label text';
+    const testDescription = 'description text';
+    const { getByText } = render(
+      <TextArea
+        description={testDescription}
+        label={testLabel}
+        name={testName}
+        onChange={() => {}}
+        value={testValue}
+      />,
+    );
+    expect(getByText(testDescription)).toBeInTheDocument();
+  });
+});
+
+describe('snapshots', () => {
+  const exampleText = 'input text';
+
+  const disabled: boolean[] = [true, false];
+  const required: (boolean | undefined)[] = [undefined, true, false];
+
+  required.forEach(isRequired => {
+    disabled.forEach(isDisabled => {
+      it(`renders an input with size disabled=${isDisabled}, required=${isRequired}`, async () => {
+        const { container } = render(
+          <TextArea
+            disabled={isDisabled}
+            errorMessage="test error"
+            label="test label"
+            name={exampleText}
+            onChange={() => {}}
+            placeholder={exampleText}
+            required={isRequired}
+            value=""
+          />,
+        );
+        expect(container.firstChild).toMatchSnapshot();
+      });
+    });
+  });
+});
---
+++ packages/react-components/form-elements/src/TextArea/index.tsx
@@ -0,0 +1,173 @@
+import { computeDataAttributes } from '@datacamp/waffles-utils';
+import { css } from '@emotion/core';
+import React, { forwardRef, ReactElement, Ref } from 'react';
+
+import { fontSizes, inputPaddings, inputStyle } from '../formStyles';
+import Label from '../Label';
+
+interface InputProps {
+  /**
+   * Sets the autocomplete attribute on the rendered input element.
+   */
+  autocomplete?: string;
+  /**
+   * Sets the css className on the rendered element. Can be used to add custom
+   * styling.
+   */
+  className?: string;
+  /**
+   * An object who's keys will be prepended with 'data-', and values will be
+   * used as attributes on the rendered element.
+   */
+  dataAttributes?: { [key: string]: string };
+  /**
+   * When provided the description will show underneath the label. Only
+   * available when 'label' is also specified.
+   */
+  description?: string;
+  /**
+   * Disables the input when true.
+   */
+  disabled?: boolean;
+  /**
+   * When provided the errorMessage will show underneath the input. Only
+   * available when label is also specified.
+   */
+  errorMessage?: string;
+  /**
+   * Sets the html id on the rendered input element.
+   */
+  id?: string;
+  /**
+   * Renders label text above the input. If this is not provided the input will
+   * render inline. When a label is provided many other props are also available
+   * and this component becomes a block that can be stacked with other form
+   * elements.
+   */
+  label?: string;
+  /**
+   * Sets the maxLength attribute on the rendered input.
+   */
+  maxLength?: number;
+  /**
+   * The minimum value of the input.
+   */
+  min?: 'string' | 'number';
+  /**
+   * Used to set the html name attribute. Uniquely indentifies the input within
+   * the current form context.
+   */
+  name: string;
+  /**
+   * Called when the user removes focus from the input.
+   */
+  onBlur?: () => void;
+  /**
+   * Called when the user requests a change to the value of the input. This
+   * should be used to set the value.
+   */
+  onChange: (value: string) => void;
+  /**
+   * The placeholder text to render before the user has entered a value.
+   */
+  placeholder?: string;
+  /**
+   * It defines wheter the input field is required or not. If required=true it
+   * adds the text 'Required' on the top-right of the input, if required=false
+   * it adds 'Optional'. The default value is undefined, which doesn't add
+   * anything. This is only available when 'label' is also provided.
+   */
+  required?: boolean;
+  /**
+   * The number of visible lines of text. This contols the height of the
+   * TextArea.
+   */
+  rows?: number;
+  /**
+   * The current value of the input. This should be controlled by listening to
+   * onChange.
+   */
+  value: string;
+}
+
+const InternalInput = ({
+  autocomplete,
+  className,
+  dataAttributes,
+  description,
+  disabled = false,
+  errorMessage,
+  id,
+  label,
+  maxLength,
+  name,
+  onBlur,
+  onChange,
+  placeholder,
+  required,
+  value,
+  innerRef,
+  rows = 2,
+}: InputProps & { innerRef?: Ref<HTMLTextAreaElement> }): ReactElement => {
+  const parsedDataAttributes = computeDataAttributes(dataAttributes);
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>): void =>
+    onChange(event.target.value);
+
+  const handleBlur = (): void => onBlur && onBlur();
+
+  const textAreaStyle = css(
+    {
+      fontSize: fontSizes.medium,
+      paddingBottom: 12,
+      paddingTop: 12,
+      resize: 'none',
+    },
+    inputPaddings.medium,
+    inputStyle,
+    label && css({ width: '100%' }),
+  );
+
+  const inputElement = (
+    <textarea
+      autoComplete={autocomplete}
+      className={className}
+      css={textAreaStyle}
+      disabled={disabled}
+      id={id}
+      maxLength={maxLength}
+      name={name}
+      onBlur={handleBlur}
+      onChange={handleChange}
+      placeholder={placeholder}
+      ref={innerRef}
+      rows={rows}
+      value={value}
+      {...parsedDataAttributes}
+    />
+  );
+
+  return (
+    <>
+      {label ? (
+        <Label
+          description={description}
+          errorMessage={errorMessage}
+          htmlFor={id}
+          label={label}
+          required={required}
+        >
+          {inputElement}
+        </Label>
+      ) : (
+        <>{inputElement}</>
+      )}
+    </>
+  );
+};
+
+const Input = forwardRef<HTMLTextAreaElement, InputProps>((props, ref) => (
+  <InternalInput innerRef={ref} {...props} />
+));
+
+export default Input;
