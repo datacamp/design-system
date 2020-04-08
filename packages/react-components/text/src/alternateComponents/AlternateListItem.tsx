3457febdc826878e1fb34b0d1d5eebbf57f54984
Louis Bailey <louis@datacamp.com>
2020-04-08 14:14:57 +0200

    fix: set arialabel on checkboxes used in markdown lists

--- packages/react-components/text/src/alternateComponents/AlternateListItem.tsx
+++ packages/react-components/text/src/alternateComponents/AlternateListItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useUIDSeed } from 'react-uid';
 
 import ListItem, { ListItemProps } from '../components/List/ListItem';
 
@@ -11,16 +12,26 @@ const AlternateListItem: React.FC<AlternateListItemProps> = ({
   children,
   checked,
 }) => {
+  const uidSeed = useUIDSeed();
+  const uid = uidSeed('checkbox-description');
   // Copied from react-markdown default renderer
   let checkbox = null;
   if (checked !== null && checked !== undefined) {
-    checkbox = <input checked={checked} disabled readOnly type="checkbox" />;
+    checkbox = (
+      <input
+        aria-labelledby={uid}
+        checked={checked}
+        disabled
+        readOnly
+        type="checkbox"
+      />
+    );
   }
 
   return (
     <ListItem>
       {checkbox}
-      {children}
+      <span id={uid}>{children}</span>
     </ListItem>
   );
 };
