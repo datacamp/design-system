75dcc682ecec9ce46e47802f883bc7979c179481
Louis Bailey <louis@datacamp.com>
2020-07-06 15:21:46 +0200

    fix: load font weights when using the components

--- packages/react-components/text/src/fontFaces/regular.ts
+++ packages/react-components/text/src/fontFaces/regular.ts
@@ -2,9 +2,9 @@ import tokens from '@datacamp/waffles-tokens/lib/future-tokens.json';
 import { css } from '@emotion/core';
 
 export default css({
-  /* latin-ext */
   '@font-face': {
     fontFamily: tokens.asset.font.sansSerif.value,
+    fontWeight: '100 900' as any,
     src:
       "url('https://waffles-beta.datacamp.com/fonts/studio-feixen-sans-variable.ttf') format('truetype')",
   },
