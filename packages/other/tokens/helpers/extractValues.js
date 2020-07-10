18dc85c189e1b45c80729a5e8ba5023827c79f04
Louis Bailey <louis@datacamp.com>
2020-07-10 09:58:53 +0200

    feat: add 900 and 650 heading sizes. Will not affect core or scss tokens

--- packages/other/tokens/helpers/extractValues.js
+++ packages/other/tokens/helpers/extractValues.js
@@ -1,3 +1,7 @@
 const _ = require('lodash');
 
-module.exports = object => _.mapValues(object, ({ value }) => value);
+module.exports = object => {
+  const newObject = object;
+  delete newObject.remove;
+  return _.mapValues(newObject, ({ value }) => value);
+};
