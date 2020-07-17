0db209f1151e5d0511313a212bbffd13b0ce6ad9
Louis Bailey <louis@datacamp.com>
2020-07-17 15:15:28 +0200

    feat: brand refresh update article

--- packages/docs/catalog-doc-site/catalog/pages/articles/overview.js
+++ packages/docs/catalog-doc-site/catalog/pages/articles/overview.js
@@ -23,6 +23,27 @@ export default () => {
           className="dc-card"
           css={{ marginTop: '24px', padding: '32px', width: '65%' }}
         >
+          <h3 className="dc-u-mb-0">Brand Refresh</h3>
+          <p>
+            As part of the brand refresh, there are a number of changes made to
+            waffles packages. This article provides a guide of how to get the
+            new branding into your applications …
+          </p>
+          <a
+            className="dc-btn dc-btn--inline"
+            href="/articles/03-brand-refresh"
+          >
+            Read Full Article
+            <ArrowRightIcon
+              css={css(arrowRightIconCss)}
+              size={arrowRightIconSize}
+            />{' '}
+          </a>
+        </article>
+        <article
+          className="dc-card"
+          css={{ marginTop: '24px', padding: '32px', width: '65%' }}
+        >
           <h3 className="dc-u-mb-0">Migrating Icons</h3>
           <p>
             All our icons have been drawn from scratch to match our brand. Up
