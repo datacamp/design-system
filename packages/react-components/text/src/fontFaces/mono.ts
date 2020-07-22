cea2bbb774d741aa796c63d5036e5eb092867b85
Louis Bailey <louis@datacamp.com>
2020-07-22 10:59:28 +0200

    feat: use JetBrains MonoNL for code font
    BREAKING CHANGE: all monospace font now uses JetBrains MonoNL

--- packages/react-components/text/src/fontFaces/mono.ts
+++ packages/react-components/text/src/fontFaces/mono.ts
@@ -7,10 +7,8 @@ const monoFont = css({
     fontFamily: tokens.asset.font.mono.value,
     fontStyle: 'normal',
     fontWeight: tokens.fontWeight.regular.value,
-    src: `url('https://waffles.datacamp.com/fonts/StudioFeixenSans-WriterRegular.eot') format('embedded-opentype'),
-        url('https://waffles.datacamp.com/fonts/StudioFeixenSans-WriterRegular.woff2') format('woff2'),
-        url('https://waffles.datacamp.com/fonts/StudioFeixenSans-WriterRegular') format('woff'),
-        url('https://waffles.datacamp.com/fonts/StudioFeixenSans-WriterRegular.ttf') format('truetype');`,
+    src: `url('https://waffles.datacamp.com/fonts/JetBrainsMonoNL-Regular.ttf')
+      format('truetype');`,
   },
 });
 
