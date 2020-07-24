6ed1cc457012c630c5e1f18ababfcaa579142020
Louis Bailey <louis@datacamp.com>
2020-07-24 14:11:48 +0200

    feat: start documenting colour changes

--- packages/docs/catalog-doc-site/catalog/pages/componentLibrary/tokens.js
+++ packages/docs/catalog-doc-site/catalog/pages/componentLibrary/tokens.js
@@ -28,19 +28,33 @@ export default () => {
             <p>
               To use any of the styles, simply import the{' '}
               <code>@datacamp/waffles-tokens</code> package and directly access
-              the necessary properties. For example if the primary color is
-              needed then it should be accessed as follows.
+              the necessary properties. For example if the green color is needed
+              then it should be accessed as follows.
             </p>
             <pre>
               <code>{`import { colors } from '@datacamp/waffles-tokens';
 
-const primaryColorValue = colors.primary;`}</code>
+const greenColorValue = colors.green;`}</code>
             </pre>
             <p>
               This hex code can then be used wherever is necessary. All the
               tokens can be accessed and used in the same way since it is just a
               json file.
             </p>
+
+            <h4>Colours</h4>
+            <p>
+              For each of the primary colours, there are 3 variations. The
+              standard colour e.g. <code>green</code> should be used as a
+              priority, but there is also a dark and a light version of every
+              colour. There is also a version to use as a text colour when being
+              used against a light coloured background, e.g.{' '}
+              <code>greenText</code>. For some colours this is the same as one
+              of the other variants, but it will always have sufficient contrast
+              against one of the lighter backgrounds. When working against a
+              dark background, the standard colours are appropriate.
+            </p>
+
             <h4>Available tokens</h4>
             <pre>
               <code>{JSON.stringify(tokens, null, 2)}</code>
