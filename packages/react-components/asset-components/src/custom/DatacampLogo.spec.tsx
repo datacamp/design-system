6346b6cf170e2ac0d8e1f8005f76413e2e49c866
Louis Bailey <louis@datacamp.com>
2020-07-27 13:43:32 +0200

    feat: make wordmark optional on DatacampLogo component

--- packages/react-components/asset-components/src/custom/DatacampLogo.spec.tsx
+++ packages/react-components/asset-components/src/custom/DatacampLogo.spec.tsx
@@ -3,8 +3,48 @@ import axeRender from '@datacamp/waffles-axe-render';
 import DatacampLogo from './DatacampLogo';
 
 describe('<DatacampLogo />', () => {
-  it(`renders the DatacampLogo with the default colours`, async () => {
+  it(`renders the DatacampLogo with the default colour`, async () => {
     const { container } = await axeRender(<DatacampLogo />);
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <svg
+        height="100"
+        width="100"
+      >
+        <g
+          fill="none"
+          fill-rule="evenodd"
+        >
+          <path
+            d="M43.7 40.513v8.333l-8.842 5.043V35.44l8.841 5.074zm2.86 17.508v-7.51l11.84-6.758-2.88-1.65-8.96 5.113V39.68a1.44 1.44 0 0 0-.718-1.242l-10.786-6.182A2.066 2.066 0 0 0 32 34.07v21.184a2.067 2.067 0 0 0 2.971 1.866l.082-.042 8.64-4.933v6.72c.002.514.276.987.721 1.244l11.088 6.29 2.88-1.65L46.56 58.02z"
+            fill="#03EF62"
+          />
+        </g>
+      </svg>
+    `);
+  });
+
+  it(`sets the colour on the logomark`, async () => {
+    const { container } = await axeRender(<DatacampLogo logomarkColor="red" />);
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <svg
+        height="100"
+        width="100"
+      >
+        <g
+          fill="none"
+          fill-rule="evenodd"
+        >
+          <path
+            d="M43.7 40.513v8.333l-8.842 5.043V35.44l8.841 5.074zm2.86 17.508v-7.51l11.84-6.758-2.88-1.65-8.96 5.113V39.68a1.44 1.44 0 0 0-.718-1.242l-10.786-6.182A2.066 2.066 0 0 0 32 34.07v21.184a2.067 2.067 0 0 0 2.971 1.866l.082-.042 8.64-4.933v6.72c.002.514.276.987.721 1.244l11.088 6.29 2.88-1.65L46.56 58.02z"
+            fill="red"
+          />
+        </g>
+      </svg>
+    `);
+  });
+
+  it(`shows the wordmark with the default colour`, async () => {
+    const { container } = await axeRender(<DatacampLogo showWordmark />);
     expect(container.firstChild).toMatchInlineSnapshot(`
       <svg
         height="100"
@@ -28,9 +68,9 @@ describe('<DatacampLogo />', () => {
     `);
   });
 
-  it(`sets the colours on the two elemnts`, async () => {
+  it(`shows the wordmark with a custom colour`, async () => {
     const { container } = await axeRender(
-      <DatacampLogo logomarkColor="red" wordmarkColor="blue" />,
+      <DatacampLogo showWordmark wordmarkColor="red" />,
     );
     expect(container.firstChild).toMatchInlineSnapshot(`
       <svg
@@ -43,12 +83,12 @@ describe('<DatacampLogo />', () => {
         >
           <path
             d="M74.56 59.099a5.694 5.694 0 1 1 0-11.389 5.694 5.694 0 0 1 0 11.389m5.704-20.622v8.852a8.334 8.334 0 1 0 0 12.15v1.834h2.632V38.477h-2.632zm73.28 20.622a5.694 5.694 0 1 1 0-11.389 5.694 5.694 0 0 1 0 11.389m8.333-5.694v-8.247h-2.63v2.171a8.334 8.334 0 1 0 0 12.15v1.834h2.631v-7.908h-.001zm20.987-7.634a1.296 1.296 0 0 1 1.109-.622h.507c1.075 0 1.947.871 1.947 1.947v14.217h-2.686V49.27c-1.239 2-5.674 9.25-7.003 11.424a1.296 1.296 0 0 1-1.108.62h-.548a1.298 1.298 0 0 1-1.298-1.297V49.238a1909.582 1909.582 0 0 0-7.31 11.954l-.074.121h-2.574v-16.16h2.684v.034l-.062 11.147 6.438-10.56a1.3 1.3 0 0 1 1.11-.622h.51c1.073 0 1.944.869 1.947 1.942 0 2.971.014 8.383.014 9.17l6.397-10.493zm-37.92 12.54a8.331 8.331 0 1 1 .21-9.502l-2.524 1.312a5.533 5.533 0 1 0-.379 6.88l2.693 1.31zm51.542.8a5.693 5.693 0 0 1-5.68-5.351v-.682a5.694 5.694 0 1 1 5.684 6.035m0-14.027a8.298 8.298 0 0 0-5.684 2.24v-2.168h-2.632V67.91h2.632v-8.4a8.333 8.333 0 1 0 5.684-14.425m-89.213 2.595v9.937c0 .59.478 1.068 1.067 1.068h3.064v2.628h-3.062a3.7 3.7 0 0 1-3.696-3.696l-.01-9.937h-2.838v-2.56h2.838v-4.418h2.635v4.427h4.672v2.55h-4.67zm12.757 11.417a5.694 5.694 0 1 1 0-11.388 5.694 5.694 0 0 1 0 11.388m5.702-13.94v2.172a8.334 8.334 0 1 0 0 12.15v1.834h2.632v-16.16l-2.632.004zM92.285 59.099a5.694 5.694 0 1 1 0-11.389 5.694 5.694 0 0 1 0 11.389m5.702-13.94v2.17a8.334 8.334 0 1 0 0 12.15v1.834h2.63v-16.16l-2.63.005z"
-            fill="blue"
+            fill="red"
             fill-rule="nonzero"
           />
           <path
             d="M43.7 40.513v8.333l-8.842 5.043V35.44l8.841 5.074zm2.86 17.508v-7.51l11.84-6.758-2.88-1.65-8.96 5.113V39.68a1.44 1.44 0 0 0-.718-1.242l-10.786-6.182A2.066 2.066 0 0 0 32 34.07v21.184a2.067 2.067 0 0 0 2.971 1.866l.082-.042 8.64-4.933v6.72c.002.514.276.987.721 1.244l11.088 6.29 2.88-1.65L46.56 58.02z"
-            fill="red"
+            fill="#03EF62"
           />
         </g>
       </svg>
