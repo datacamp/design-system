41aa718d728da49adbd50a29c1bc68e0baecd600
Louis Bailey <louis@datacamp.com>
2020-08-13 14:37:21 +0200

    feat: update cards to reflect the new branding
    BREAKING CHANGE: cards now use the new navy colour for shadows and 
    borders

--- packages/react-components/card/src/index.spec.tsx
+++ packages/react-components/card/src/index.spec.tsx
@@ -66,7 +66,7 @@ describe('<Card />', () => {
       );
 
       expect(container.firstChild).toHaveStyle(
-        `box-shadow: ${shadow.border.value}`,
+        `box-shadow: ${shadow.border.value};`,
       );
     });
 
@@ -78,7 +78,7 @@ describe('<Card />', () => {
       );
 
       expect(container.firstChild).toHaveStyle(
-        `box-shadow: ${shadow.border.value}`,
+        `box-shadow: ${shadow.border.value};`,
       );
     });
 
@@ -90,7 +90,7 @@ describe('<Card />', () => {
       );
 
       expect(container.firstChild).toHaveStyle(
-        `box-shadow: 0px 0px 1px 0px rgba(61,66,81,0.3),${shadow.sm.value}`,
+        `box-shadow: 0px 0px 1px 0px rgba(5,25,45,0.3),${shadow.sm.value};`,
       );
     });
 
@@ -102,7 +102,7 @@ describe('<Card />', () => {
       );
 
       expect(container.firstChild).toHaveStyle(
-        `box-shadow: 0px 0px 1px 0px rgba(61,66,81,0.3),${shadow.md.value}`,
+        `box-shadow: 0px 0px 1px 0px rgba(5,25,45,0.3),${shadow.md.value};`,
       );
     });
 
@@ -114,7 +114,7 @@ describe('<Card />', () => {
       );
 
       expect(container.firstChild).toHaveStyle(
-        `box-shadow: 0px 0px 1px 0px rgba(61,66,81,0.3),${shadow.lg.value}`,
+        `box-shadow: 0px 0px 1px 0px rgba(5,25,45,0.3),${shadow.lg.value};`,
       );
     });
 
@@ -126,7 +126,7 @@ describe('<Card />', () => {
       );
 
       expect(container.firstChild).toHaveStyle(
-        `box-shadow: 0px 0px 1px 0px rgba(61,66,81,0.3),${shadow.xl.value}`,
+        `box-shadow: 0px 0px 1px 0px rgba(5,25,45,0.3),${shadow.xl.value};`,
       );
     });
   });
