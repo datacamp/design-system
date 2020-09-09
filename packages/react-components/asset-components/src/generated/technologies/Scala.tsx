384e8aa71f5d54ddf92d8e05068f0e3efa6ebd06
Kristof Houben <kristof@datacamp.com>
2020-09-09 13:20:34 +0200

    feat: updated tech icons

--- packages/react-components/asset-components/src/generated/technologies/Scala.tsx
+++ packages/react-components/asset-components/src/generated/technologies/Scala.tsx
@@ -2,26 +2,13 @@ import * as React from 'react';
 
 function SvgScala(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg width={39} height={39} {...props}>
-      <defs>
-        <linearGradient
-          id="scala_svg__a"
-          x1="50%"
-          x2="50%"
-          y1="17.738%"
-          y2="100%"
-        >
-          <stop offset="0%" stopColor="#FF7876" />
-          <stop offset="100%" stopColor="#C5354B" />
-        </linearGradient>
-        <circle id="scala_svg__b" cx={19.5} cy={19.5} r={19.5} />
-      </defs>
+    <svg viewBox="0 0 39 39" {...props}>
       <g fill="none" fillRule="evenodd">
-        <use fill="url(#scala_svg__a)" xlinkHref="#scala_svg__b" />
+        <circle cx={19.5} cy={19.5} r={19.5} fill="#05192D" />
         <path
-          d="M26.292 7v5.317s0 2.215-13.292 3.545v-5.317S26.292 9.215 26.292 7M13 17.634s13.292-1.33 13.292-3.545v5.317s0 2.216-13.292 3.545v-5.317zm0 12.406v-5.317s13.292-1.33 13.292-3.545v5.317s0 2.216-13.292 3.545"
           fill="#FFF"
           fillRule="nonzero"
+          d="M26.292 7v5.317s0 2.215-13.292 3.545v-5.317S26.292 9.215 26.292 7M13 17.634s13.292-1.33 13.292-3.545v5.317s0 2.216-13.292 3.545v-5.317zm0 12.406v-5.317s13.292-1.33 13.292-3.545v5.317s0 2.216-13.292 3.545"
         />
       </g>
     </svg>
--- packages/react-components/asset-components/src/generated/technologies/Shell.tsx
+++ packages/react-components/asset-components/src/generated/technologies/Shell.tsx
@@ -2,29 +2,12 @@ import * as React from 'react';
 
 function SvgShell(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg width={39} height={39} {...props}>
-      <defs>
-        <linearGradient
-          id="shell_svg__b"
-          x1="50%"
-          x2="50%"
-          y1="98.012%"
-          y2="5.507%"
-        >
-          <stop offset="0%" stopColor="#405F8F" />
-          <stop offset="100%" stopColor="#86A9DF" />
-        </linearGradient>
-        <circle id="shell_svg__a" cx={19.5} cy={19.5} r={19.5} />
-      </defs>
+    <svg viewBox="0 0 39 39" {...props}>
       <g fill="none" fillRule="evenodd">
-        <mask id="shell_svg__c" fill="#fff">
-          <use xlinkHref="#shell_svg__a" />
-        </mask>
-        <use fill="url(#shell_svg__b)" xlinkHref="#shell_svg__a" />
+        <circle cx={19.5} cy={19.5} r={19.5} fill="#05192D" />
         <path
           fill="#FFF"
           d="M30.269 21.537v3.202h-11.06v-3.202h11.06zM9.022 12.806l9.72 4.041v2.534l-9.72 4.05v-3.022l5.908-2.286-5.908-2.278v-3.039z"
-          mask="url(#shell_svg__c)"
         />
       </g>
     </svg>
