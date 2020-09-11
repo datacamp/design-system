4d60869c40e77e1c779625fadb70709a9db9e63d
Louis Bailey <louis@datacamp.com>
2020-09-11 15:09:12 +0200

    feat: add ALPALoop component

---
+++ packages/react-components/asset-components/src/custom/ALPALoop.spec.tsx
@@ -0,0 +1,12 @@
+import axeRender from '@datacamp/waffles-axe-render';
+
+import ALPALoop, { Highlight, loopComponents } from './ALPALoop';
+
+describe('<ALPALoop />', () => {
+  (Object.keys(loopComponents) as Highlight[]).forEach(highlight => {
+    it(`renders a ${highlight} logo`, async () => {
+      const { container } = await axeRender(<ALPALoop highlight={highlight} />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
+  });
+});
