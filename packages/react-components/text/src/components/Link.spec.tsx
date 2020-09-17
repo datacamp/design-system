491bb28be7da072b1266ab008677aabbc4edda7c
Louis Bailey <louis@datacamp.com>
2020-09-17 14:36:00 +0200

    feat: add onClick and onMouseEnter to Link component

--- packages/react-components/text/src/components/Link.spec.tsx
+++ packages/react-components/text/src/components/Link.spec.tsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 
 import axeRender from '@datacamp/waffles-axe-render';
+import { fireEvent } from '@testing-library/react';
 import React from 'react';
 
 import Link from './Link';
@@ -53,5 +54,23 @@ describe('<Text />', () => {
       'data-example',
       testDataAttributes.example,
     );
+  });
+
+  it('fires onClick', async () => {
+    const onClick = jest.fn();
+    const { getByText } = await axeRender(
+      <Link onClick={onClick}>{testText}</Link>,
+    );
+    fireEvent.click(getByText(testText));
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('fires onMouseEnter', async () => {
+    const onMouseEnter = jest.fn();
+    const { getByText } = await axeRender(
+      <Link onMouseEnter={onMouseEnter}>{testText}</Link>,
+    );
+    fireEvent.mouseEnter(getByText(testText));
+    expect(onMouseEnter).toHaveBeenCalledTimes(1);
   });
 });
