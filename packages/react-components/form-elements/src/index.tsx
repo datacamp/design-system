f090e8a835300de659c46f67044f4b54df9be6a7
Louis Bailey <louis@datacamp.com>
2020-10-02 11:37:32 +0200

    feat: expose RadioIcon for custom elements

--- packages/react-components/form-elements/src/index.tsx
+++ packages/react-components/form-elements/src/index.tsx
@@ -4,6 +4,7 @@ export { default as SelectOption } from './Select/Option';
 export { default as RadioList } from './RadioList';
 export { default as CheckboxList } from './CheckboxList';
 export { default as Radio } from './RadioList/Radio';
+export { default as RadioIcon } from './RadioList/RadioIcon';
 export { default as Checkbox } from './CheckboxList/Checkbox';
 export { default as Label } from './Label';
 export { default as TextArea } from './TextArea';
