4e2f9d3d0966bb17b3ed99c9c75b7402a62dbc3e
Louis Bailey <louis@datacamp.com>
2020-11-20 11:43:56 +0100

    feat: remove topic logos
    BREAKING CHANGE: Topic logos have been deleted

--- packages/react-components/asset-components/src/index.ts
+++ packages/react-components/asset-components/src/index.ts
@@ -2,7 +2,6 @@ import * as ALPALogos from './generated/alpa';
 import * as Datacamp from './generated/logos/datacamp';
 import * as Waffles from './generated/logos/waffles';
 import * as TechnologiesLogos from './generated/technologies';
-import * as TopicsLogos from './generated/topics';
 
 export { default as ALPALoop } from './custom/ALPALoop';
 export { default as TechLogo } from './custom/TechLogo';
@@ -10,5 +9,4 @@ export { default as DatacampLogo } from './custom/DatacampLogo';
 export const Logos = { Datacamp, Waffles };
 
 export const Technologies = TechnologiesLogos;
-export const Topics = TopicsLogos;
 export const ALPA = ALPALogos;
