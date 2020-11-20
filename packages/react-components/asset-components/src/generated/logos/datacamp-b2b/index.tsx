5694d0511365362bb8fe66435138fcaa156a4328
Louis Bailey <louis@datacamp.com>
2020-11-20 11:38:21 +0100

    fix: remove datacamp b2b logos
    BREAKING CHANGE: The DataCamp B2B logos have been deleted

--- packages/react-components/asset-components/src/generated/logos/datacamp-b2b/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as Inverted } from './Inverted'
-export { default as Regular } from './Regular'
-export { default as SingleColor } from './SingleColor'
--- packages/react-components/asset-components/src/index.ts
+++ packages/react-components/asset-components/src/index.ts
@@ -1,6 +1,5 @@
 import * as ALPALogos from './generated/alpa';
 import * as Datacamp from './generated/logos/datacamp';
-import * as DatacampB2B from './generated/logos/datacamp-b2b';
 import * as Waffles from './generated/logos/waffles';
 import * as TechnologiesLogos from './generated/technologies';
 import * as TopicsLogos from './generated/topics';
@@ -8,7 +7,7 @@ import * as TopicsLogos from './generated/topics';
 export { default as ALPALoop } from './custom/ALPALoop';
 export { default as TechLogo } from './custom/TechLogo';
 export { default as DatacampLogo } from './custom/DatacampLogo';
-export const Logos = { Datacamp, DatacampB2B, Waffles };
+export const Logos = { Datacamp, Waffles };
 
 export const Technologies = TechnologiesLogos;
 export const Topics = TopicsLogos;
