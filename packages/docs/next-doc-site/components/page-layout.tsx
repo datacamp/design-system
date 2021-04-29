974c38340745d120ba099bd2b2014838c9bb1aef
Mateusz Łachacz <info@blurbyte.com>
2021-04-29 18:06:44 +0200

    feat: implement generic page layout component

---
+++ packages/docs/next-doc-site/components/page-layout.tsx
@@ -0,0 +1,32 @@
+// eslint-disable-next-line filenames/match-exported
+import { colors } from '@datacamp/waffles-tokens';
+import { css } from '@emotion/react';
+import { MDXProvider } from '@mdx-js/react';
+import { Fragment } from 'react';
+
+import { SIDEBAR_WIDTH } from './constants';
+import MarkdownElements from './markdown-elements';
+import Sidebar from './sidebar';
+
+const mainStyle = css`
+  padding-left: ${SIDEBAR_WIDTH}px;
+  background-color: ${colors.beige100};
+  min-height: 100vh;
+`;
+
+type PageLayoutProps = {
+  children: React.ReactNode;
+};
+
+function PageLayout({ children }: PageLayoutProps): JSX.Element {
+  return (
+    <Fragment>
+      <Sidebar />
+      <main css={mainStyle}>
+        <MDXProvider components={MarkdownElements}>{children}</MDXProvider>
+      </main>
+    </Fragment>
+  );
+}
+
+export default PageLayout;
