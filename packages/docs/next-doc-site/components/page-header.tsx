5e18dce01b07db4536f9852962093033abcaa8e7
Mateusz Łachacz <info@blurbyte.com>
2021-04-29 18:07:13 +0200

    feat: create reusable page header and section

---
+++ packages/docs/next-doc-site/components/page-header.tsx
@@ -0,0 +1,67 @@
+// eslint-disable-next-line filenames/match-exported
+import { Heading, Paragraph } from '@datacamp/waffles-text';
+import { colors, fontSize, fontWeight } from '@datacamp/waffles-tokens';
+import { css } from '@emotion/react';
+import Head from 'next/head';
+import { Fragment } from 'react';
+
+import { MAX_CONTENT_WIDTH } from './constants';
+
+const headerStyle = css`
+  background-color: ${colors.navy};
+  width: 100%;
+  padding: 32px;
+  padding-top: 48px;
+`;
+
+const contentStyle = css`
+  max-width: ${MAX_CONTENT_WIDTH}px;
+`;
+
+const categoryStyle = css`
+  color: ${colors.green};
+  font-weight: ${fontWeight.bold};
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-size: ${fontSize.small};
+`;
+
+const titleStyle = css`
+  color: ${colors.white};
+  margin-top: 4px !important;
+`;
+
+const overviewStyle = css`
+  color: ${colors.white};
+`;
+
+type PageHeaderProps = {
+  category: string;
+  overview: string;
+  pageTitle: string;
+};
+
+function PageHeader({
+  category,
+  overview,
+  pageTitle,
+}: PageHeaderProps): JSX.Element {
+  return (
+    <Fragment>
+      <Head>
+        <title key="title">Waffles - {pageTitle}</title>
+      </Head>
+      <header css={headerStyle}>
+        <div css={contentStyle}>
+          <Paragraph css={categoryStyle}>{category}</Paragraph>
+          <Heading as="h1" css={titleStyle} multiLine size={700}>
+            {pageTitle}
+          </Heading>
+          <Paragraph css={overviewStyle}>{overview}</Paragraph>
+        </div>
+      </header>
+    </Fragment>
+  );
+}
+
+export default PageHeader;
---
+++ packages/docs/next-doc-site/components/page-section.tsx
@@ -0,0 +1,34 @@
+// eslint-disable-next-line filenames/match-exported
+import Card from '@datacamp/waffles-card';
+import { css } from '@emotion/react';
+
+import { MAX_CONTENT_WIDTH } from './constants';
+
+const sectionStyle = css`
+  padding: 16px 32px 0 32px;
+  max-width: ${MAX_CONTENT_WIDTH}px;
+
+  &:first-of-type {
+    padding-top: 32px;
+  }
+`;
+
+const cardStyle = css`
+  padding: 16px;
+`;
+
+type PageSectionProps = {
+  children: React.ReactNode;
+};
+
+function PageSection({ children }: PageSectionProps): JSX.Element {
+  return (
+    <section css={sectionStyle}>
+      <Card css={cardStyle} elevation={2}>
+        {children}
+      </Card>
+    </section>
+  );
+}
+
+export default PageSection;
