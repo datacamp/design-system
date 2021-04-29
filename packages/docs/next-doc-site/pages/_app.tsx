ab201550acb55b3cc3f4a420dcb002946bdeb1e3
Mateusz Łachacz <info@blurbyte.com>
2021-04-29 18:03:13 +0200

    feat: update nextjs app and document

--- packages/docs/next-doc-site/pages/_app.tsx
+++ packages/docs/next-doc-site/pages/_app.tsx
@@ -1,17 +1,8 @@
 /* eslint-disable filenames/match-exported */
 import { AppProps } from 'next/app';
-import Head from 'next/head';
-import React from 'react';
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
-  return (
-    <>
-      <Head>
-        <title>Waffles - The DataCamp Design System</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default App;
