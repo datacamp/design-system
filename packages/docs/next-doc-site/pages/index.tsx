69369bb9d181d78190d2e2102e09dfd7ec628d3e
Mateusz Łachacz <info@blurbyte.com>
2021-04-29 18:07:27 +0200

    feat: create example pages

--- packages/docs/next-doc-site/pages/index.tsx
+++ packages/docs/next-doc-site/pages/index.tsx
@@ -1,16 +1,24 @@
 /* eslint-disable filenames/match-exported */
-import { Heading, Paragraph } from '@datacamp/waffles-text';
-import { colors } from '@datacamp/waffles-tokens';
+import Markdown from '../components/markdown-elements';
+import Header from '../components/page-header';
+import Layout from '../components/page-layout';
+import Section from '../components/page-section';
 
-function Home(): JSX.Element {
+function Welcome(): JSX.Element {
   return (
-    <main css={{ backgroundColor: colors.beige400 }}>
-      <Heading as="h1" size={800}>
-        Waffles Documentation
-      </Heading>
-      <Paragraph>This is the new waffles doc site</Paragraph>
-    </main>
+    <Layout>
+      <Header
+        category="Welcome"
+        overview="Waffles provides all the tools you need to build wonderful experiences that look, feel, sound and smell like DataCamp. They are also our favourite snack."
+        pageTitle="Waffles Is DataCamp Design System"
+      />
+      <Section>
+        <Markdown.h2>New content incoming</Markdown.h2>
+        <Markdown.p>This is the new waffles doc site.</Markdown.p>
+        <Markdown.p>Some extra content below.</Markdown.p>
+      </Section>
+    </Layout>
   );
 }
 
-export default Home;
+export default Welcome;
