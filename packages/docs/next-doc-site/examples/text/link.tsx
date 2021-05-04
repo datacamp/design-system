1f1fd75f72b7d80ec2d57c6bbc2530cf6f4aabac
Mateusz Łachacz <info@blurbyte.com>
2021-05-04 18:02:56 +0200

    feat: add dummy section about text components

---
+++ packages/docs/next-doc-site/examples/text/link.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line filenames/match-exported
+import { Link } from '@datacamp/waffles-text';
+
+function Example(): JSX.Element {
+  return (
+    <Link href="https://datacamp.com">
+      A simple link to the DataCamp website.
+    </Link>
+  );
+}
+
+export default Example;
---
+++ packages/docs/next-doc-site/examples/text/paragraph.tsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line filenames/match-exported
+import { Paragraph } from '@datacamp/waffles-text';
+
+function Example(): JSX.Element {
+  return <Paragraph>Just a basic text.</Paragraph>;
+}
+
+export default Example;
