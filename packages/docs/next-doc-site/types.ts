4303b222f9c32627c899c695a7eace290f88c2a8
Mateusz Łachacz <info@blurbyte.com>
2021-05-07 23:08:17 +0200

    feat: add types for components metadata

--- packages/docs/next-doc-site/types.ts
+++ packages/docs/next-doc-site/types.ts
@@ -3,3 +3,38 @@ export type PlaygroundConfig = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   scope?: { [key: string]: any };
 };
+
+export type Metadata = {
+  [key: string]: SingleComponentMetadata[];
+};
+
+export type SingleComponentMetadata = {
+  description?: string;
+  displayName: string;
+  props?: {
+    [key: string]: PropMetadata;
+  };
+};
+
+export type PropMetadata = {
+  defaultValue?: {
+    value: string;
+  };
+  description: string;
+  required: boolean;
+  tsType?: {
+    elements?: Array<{
+      name: string;
+      value?: string;
+    }>;
+    name: string;
+    type?: string;
+  };
+  type?: {
+    name: string;
+    value:
+      | Array<{ value: string }>
+      | Array<{ name: string }>
+      | { name: string };
+  };
+};
