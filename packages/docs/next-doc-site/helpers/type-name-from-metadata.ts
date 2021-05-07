13262a4c3e6286cf82f01725d982672912dd00a6
Mateusz Łachacz <info@blurbyte.com>
2021-05-07 23:11:37 +0200

    feat: create function to get common type namesfrom metadata

---
+++ packages/docs/next-doc-site/helpers/type-name-from-metadata.ts
@@ -0,0 +1,45 @@
+// eslint-disable-next-line filenames/match-exported
+import { PropMetadata } from '../types';
+
+// It's pretty dirty, but in future we won't it
+function typeNameFromMetadata(prop: PropMetadata): string {
+  // First try type from propTypes
+  if (prop.type) {
+    const { type } = prop;
+    switch (type.name) {
+      case 'enum': {
+        const value = type.value as Array<{ value: string }>;
+        return value.map((item) => item.value).join(' | ');
+      }
+      case 'union': {
+        const value = type.value as Array<{ name: string }>;
+        return value.map((item) => item.name).join(' | ');
+      }
+      case 'arrayOf': {
+        const value = type.value as { name: string };
+        return `arrayOf(${value.name})`;
+      }
+      case 'func':
+        return 'function';
+      default:
+        return type.name;
+    }
+  }
+
+  // Then try type from typescript
+  if (prop.tsType) {
+    const { tsType } = prop;
+
+    if (tsType.elements) {
+      return tsType.elements
+        .map((element) => element.value || element.name)
+        .join(' | ');
+    }
+
+    return tsType.type || tsType.name;
+  }
+
+  return '⚠️ Unknown type';
+}
+
+export default typeNameFromMetadata;
