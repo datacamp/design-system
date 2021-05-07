aebb6a75ac22398e2a086492030c41e68a8183f1
Mateusz Łachacz <info@blurbyte.com>
2021-05-07 23:10:30 +0200

    feat: implement function to get metadata for single component

---
+++ packages/docs/next-doc-site/helpers/component-metadata-by-name.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line filenames/match-exported
+import { Metadata, SingleComponentMetadata } from '../types';
+
+function componentMetadataByName(
+  metadata: Metadata,
+  name: string,
+): SingleComponentMetadata | undefined {
+  const data = Object.values(metadata).find(
+    (item) => item[0].displayName === name,
+  );
+  return data && data[0];
+}
+
+export default componentMetadataByName;
