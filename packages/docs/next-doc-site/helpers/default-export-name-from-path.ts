74f5b40bd8679655b30fd3802488ef21b2e396bb
Mateusz Łachacz <info@blurbyte.com>
2021-05-12 21:11:11 +0200

    feat: implement utility to create default export name from path

---
+++ packages/docs/next-doc-site/helpers/default-export-name-from-path.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line filenames/match-exported
+function defaultExportNameFromPath(path: string): string {
+  const nameKebabCase = path.split('waffles-')[1];
+  // Convert name to pascal case
+  return nameKebabCase
+    .split('-')
+    .map((part) => {
+      return `${part.charAt(0).toUpperCase()}${part.slice(1)}`;
+    })
+    .join();
+}
+
+export default defaultExportNameFromPath;
