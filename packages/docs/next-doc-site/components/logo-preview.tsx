605d80c086049be76b83e53d4f512bd24972adaf
Mateusz Łachacz <info@blurbyte.com>
2021-05-13 13:25:43 +0200

    feat: implement logo-preview component

---
+++ packages/docs/next-doc-site/components/logo-preview.tsx
@@ -0,0 +1,58 @@
+// eslint-disable-next-line filenames/match-exported
+import { css } from '@emotion/react';
+
+import DownloadButton from './download-button';
+import Markdown from './markdown-elements';
+import Preview from './preview';
+
+const wrapperStyle = css`
+  margin-top: 16px;
+
+  &:not(:first-of-type) {
+    margin-top: 24px;
+  }
+`;
+
+const prevewStyle = css`
+  padding: 16px;
+  justify-content: center;
+  margin-bottom: 16px;
+`;
+
+const buttonStyle = css`
+  margin-top: 16px;
+`;
+
+type LogoPreview = {
+  children: React.ReactNode;
+  description: string;
+  fileName: string;
+  isInverted?: boolean;
+  title?: string;
+};
+
+function LogoPreview({
+  children,
+  fileName,
+  isInverted = false,
+  title,
+}: LogoPreview): JSX.Element {
+  return (
+    <section css={wrapperStyle}>
+      {title && <Markdown.h3>{title}</Markdown.h3>}
+      <Preview css={prevewStyle} isInverted={isInverted}>
+        <img alt={`DataCamp Logo - ${title}`} src={`/logo/${fileName}.svg`} />
+      </Preview>
+      {children}
+      <DownloadButton
+        css={buttonStyle}
+        download={fileName}
+        href={`/downloads/${fileName}.zip`}
+      >
+        Download for Print and Web
+      </DownloadButton>
+    </section>
+  );
+}
+
+export default LogoPreview;
