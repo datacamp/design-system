e4478f61685b3de5e8524d83a3f336f12d6448a7
Mateusz Łachacz <info@blurbyte.com>
2021-05-18 15:14:00 +0200

    feat: implement all-icons-grid component

---
+++ packages/docs/next-doc-site/components/all-icons-grid.tsx
@@ -0,0 +1,41 @@
+// eslint-disable-next-line filenames/match-exported
+import * as allIcons from '@datacamp/waffles-icons';
+import { Text } from '@datacamp/waffles-text';
+import { css } from '@emotion/react';
+
+import Preview from './preview';
+
+const previewStyle = css`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 8px;
+`;
+
+const iconStyle = css`
+  display: flex;
+  align-items: center;
+  margin: 8px;
+`;
+
+const textStyle = css`
+  padding-left: 8px;
+`;
+
+function AllIconsGrid(): JSX.Element {
+  return (
+    <Preview css={previewStyle}>
+      {Object.entries(allIcons).map((iconData) => {
+        const [name, Icon] = iconData;
+
+        return (
+          <div css={iconStyle} key={`$icon-${name}`}>
+            <Icon />
+            <Text css={textStyle}>{name}</Text>
+          </div>
+        );
+      })}
+    </Preview>
+  );
+}
+
+export default AllIconsGrid;
