dd9753e06655893762f397c9a7f293ddd670bed4
Mateusz Łachacz <info@blurbyte.com>
2021-05-25 12:56:55 +0200

    feat: add multiple switches example

---
+++ packages/docs/next-doc-site/examples/form-elements/multiple-switches.tsx
@@ -0,0 +1,57 @@
+import { Switch } from '@datacamp/waffles-form-elements';
+import tokens from '@datacamp/waffles-tokens';
+import { useState } from 'react';
+
+type ExampleState = {
+  [key: string]: boolean;
+};
+
+function Example(): JSX.Element {
+  const [settings, setSettings] = useState<ExampleState>({
+    airplane: true,
+    bluetooth: false,
+    wifi: false,
+  });
+
+  function handleChange(event: React.FormEvent<HTMLInputElement>): void {
+    const { name } = event.target as HTMLInputElement;
+
+    const newSettings = {
+      ...settings,
+      [name]: !settings[name],
+    };
+
+    setSettings(newSettings);
+  }
+
+  return (
+    <form css={{ display: 'flex', flexWrap: 'wrap', gap: 24 }}>
+      <Switch
+        appearance="inverted"
+        checked={settings.bluetooth}
+        css={{
+          backgroundColor: tokens.colors.navy,
+          borderRadius: tokens.border.radius,
+          padding: 8,
+        }}
+        name="bluetooth"
+        onChange={handleChange}
+      >
+        Bluetooth
+      </Switch>
+      <Switch checked={settings.wifi} name="wifi" onChange={handleChange}>
+        Wi-Fi
+      </Switch>
+      <Switch
+        checked={settings.airplane}
+        disabled
+        name="airplane"
+        onChange={handleChange}
+      >
+        Airplane mode
+      </Switch>
+    </form>
+  );
+}
+
+export default Example;
