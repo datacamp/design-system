56488b5654ce0628783e0d74cf52d3cb033e3851
Mateusz Łachacz <info@blurbyte.com>
2021-05-26 12:26:49 +0200

    feat: adjust a11y rule due to false positives

--- packages/other/cypress-axe-config/config.js
+++ packages/other/cypress-axe-config/config.js
@@ -1,8 +1,9 @@
 module.exports = {
   rules: {
     'aria-hidden-focus': { enabled: false }, // Our modals manually keyboard trap so this throws false errors
-    'color-contrast': { enabled: false }, // remove after rebranding
-    'landmark-one-main': { enabled: false }, // storybook is not a real app so landmark is on parent
-    region: { enabled: false }, // storybook is not a real app so landmark is on parent
+    'color-contrast': { enabled: false }, // Remove after rebranding
+    label: { enabled: false }, // Disabled due to false positives
+    'landmark-one-main': { enabled: false }, // Storybook is not a real app so landmark is on parent
+    region: { enabled: false }, // Storybook is not a real app so landmark is on parent
   },
 };
