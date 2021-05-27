bb54e1d13bb5c866deda29d880667a67bc489de4
Mateusz Łachacz <info@blurbyte.com>
2021-05-27 14:49:07 +0200

    feat: remove lodash from utils

--- packages/other/utils/src/computeDataAttributes.ts
+++ packages/other/utils/src/computeDataAttributes.ts
@@ -1,12 +1,27 @@
-import { mapKeys } from 'lodash';
+type DataAttributes = {
+  [key: string]: string;
+};
 
 /**
  * Prepends each key of an object with 'data-'
  */
-const computeDataAttributes = (dataAttributes?: {
-  [key: string]: string;
-}): { [key: string]: string } => {
-  return mapKeys(dataAttributes, (_, key) => `data-${key}`);
+const computeDataAttributes = (
+  dataAttributes?: DataAttributes,
+): DataAttributes => {
+  if (!dataAttributes) {
+    return {};
+  }
+
+  return Object.entries(dataAttributes).reduce<DataAttributes>(
+    (updatedEntries, entry) => {
+      const [key, value] = entry;
+      return {
+        ...updatedEntries,
+        [`data-${key}`]: value,
+      };
+    },
+    {},
+  );
 };
 
 export default computeDataAttributes;
