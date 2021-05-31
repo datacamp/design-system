e40fd03b5d2e48671bea141329e328848daa75a3
Mateusz Łachacz <info@blurbyte.com>
2021-05-31 14:15:27 +0200

    feat: add HEX color check

--- packages/other/utils/src/hexToRgbaColor.ts
+++ packages/other/utils/src/hexToRgbaColor.ts
@@ -1,8 +1,16 @@
+const hexRegex = /^#([a-fA-F0-9]{3}){1,2}$/;
+
 /**
  * Converts HEX color to RGBA counterpart, with optional alpha setting
  */
-const hexToRgbaColor = (hex: string, alpha = 1): string => {
-  const value = hex.slice(1);
+const hexToRgbaColor = (hexColor: string, alpha = 1): string => {
+  if (!hexRegex.test(hexColor)) {
+    throw new Error(
+      "Couldn't parse the color string. Please provide the color as a string in HEX notation.",
+    );
+  }
+
+  const value = hexColor.slice(1);
   const group = value.length / 3;
   return `rgba(${[0, 1, 2]
     .map((i) => value.slice(i * group, (i + 1) * group))
