6177a40a69ff1752d004c60dff8b576f63078905
Mateusz Łachacz <info@blurbyte.com>
2021-05-31 14:16:59 +0200

    feat: export utility and update lockfile

--- packages/other/utils/src/index.ts
+++ packages/other/utils/src/index.ts
@@ -3,3 +3,4 @@ export { default as isChildType } from './isChildType';
 export { default as ssrSafeNotFirstChildSelector } from './ssrSafeNotFirstChildSelector';
 export { default as childrenOfType } from './childrenOfType';
 export { default as hexToRgbaColor } from './hexToRgbaColor';
+export { default as hexColorLuminance } from './hexColorLuminance';
