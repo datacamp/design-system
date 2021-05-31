0b3d348c81b4f9997c39148cf98d8a2bfb2e08a2
Mateusz Łachacz <info@blurbyte.com>
2021-05-31 14:17:36 +0200

    chore: add tests and update snapshots

--- packages/other/utils/src/hexToRgbaColor.spec.ts
+++ packages/other/utils/src/hexToRgbaColor.spec.ts
@@ -3,7 +3,7 @@ import hexToRgbaColor from './hexToRgbaColor';
 
 describe('hexToRgbaColor', () => {
   it('converts HEX color value to RGB counterpart when opacity is not provided', () => {
-    expect(hexToRgbaColor('#0033ff')).toEqual('rgba(0, 51, 255, 1)');
+    expect(hexToRgbaColor('#0033FF')).toEqual('rgba(0, 51, 255, 1)');
     expect(hexToRgbaColor('#03f')).toEqual('rgba(0, 51, 255, 1)');
     expect(hexToRgbaColor('#03ef62')).toEqual('rgba(3, 239, 98, 1)');
   });
@@ -14,7 +14,30 @@ describe('hexToRgbaColor', () => {
     expect(hexToRgbaColor('#03f', 1)).toEqual('rgba(0, 51, 255, 1)');
     expect(hexToRgbaColor('#03f', 0)).toEqual('rgba(0, 51, 255, 0)');
     expect(hexToRgbaColor('#03ef62', 0.3)).toEqual('rgba(3, 239, 98, 0.3)');
-    expect(hexToRgbaColor('#03ef62', 1)).toEqual('rgba(3, 239, 98, 1)');
+    expect(hexToRgbaColor('#03EF62', 1)).toEqual('rgba(3, 239, 98, 1)');
     expect(hexToRgbaColor('#03ef62', 0)).toEqual('rgba(3, 239, 98, 0)');
+  });
+
+  it('throws an error when icorrect HEX value is provided', () => {
+    const ERROR_MESSAGE =
+      "Couldn't parse the color string. Please provide the color as a string in HEX notation.";
+    expect(() => {
+      hexToRgbaColor('abc');
+    }).toThrow(ERROR_MESSAGE);
+    expect(() => {
+      hexToRgbaColor('');
+    }).toThrow(ERROR_MESSAGE);
+    expect(() => {
+      hexToRgbaColor('#');
+    }).toThrow(ERROR_MESSAGE);
+    expect(() => {
+      hexToRgbaColor('#ABC1');
+    }).toThrow(ERROR_MESSAGE);
+    expect(() => {
+      hexToRgbaColor('#AB');
+    }).toThrow(ERROR_MESSAGE);
+    expect(() => {
+      hexToRgbaColor('#abb34a7');
+    }).toThrow(ERROR_MESSAGE);
   });
 });
