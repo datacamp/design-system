f7cc5c919379c48c32dcf119a2dafea2015ba07e
Mateusz Łachacz <info@blurbyte.com>
2021-06-04 21:37:46 +0200

    feat: set some mandatory menu items

--- packages/react-components/button/src/UserAccountMenuButton/index.tsx
+++ packages/react-components/button/src/UserAccountMenuButton/index.tsx
@@ -2,18 +2,8 @@ import { CogIcon, ExitIcon, UserIcon } from '@datacamp/waffles-icons';
 import tokens from '@datacamp/waffles-tokens';
 import { css } from '@emotion/react';
 
-import Button from './Button';
-import Dropdown from './Dropdown';
+import Menu from './Menu';
 import MenuItem from './MenuItem';
-import useMenu from './useMenu';
-import XpIndicator from './XpIndicator';
-
-const wrapperStyle = css({
-  alignItems: 'center',
-  display: 'inline-flex',
-  height: '100%',
-  position: 'relative',
-});
 
 const logoutStyle = css({
   borderTop: `1px solid ${tokens.colors.beige400}`,
@@ -23,39 +13,22 @@ const logoutStyle = css({
 });
 
 function UserAccountMenuButton(): JSX.Element {
-  const { buttonProps, isOpen, itemProps } = useMenu(3);
-
   return (
-    <div css={wrapperStyle}>
-      <Button isOpen={isOpen} {...buttonProps} />
-      {isOpen && (
-        <Dropdown>
-          <XpIndicator />
-          <MenuItem
-            href="https://www.datacamp.com"
-            icon={UserIcon}
-            {...itemProps[0]}
-          >
-            My Profile
-          </MenuItem>
-          <MenuItem
-            href="https://www.datacamp1.com"
-            icon={CogIcon}
-            {...itemProps[1]}
-          >
-            Account Settings
-          </MenuItem>
-          <MenuItem
-            css={logoutStyle}
-            href="https://www.datacamp2.com"
-            icon={ExitIcon}
-            {...itemProps[2]}
-          >
-            Log Out
-          </MenuItem>
-        </Dropdown>
-      )}
-    </div>
+    <Menu>
+      <MenuItem href="https://www.datacamp.com" icon={UserIcon}>
+        My Profile
+      </MenuItem>
+      <MenuItem href="https://www.datacamp1.com" icon={CogIcon}>
+        Account Settings
+      </MenuItem>
+      <MenuItem
+        css={logoutStyle}
+        href="https://www.datacamp2.com"
+        icon={ExitIcon}
+      >
+        Log Out
+      </MenuItem>
+    </Menu>
   );
 }
 
