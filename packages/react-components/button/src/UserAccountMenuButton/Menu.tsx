33155794b500d91d747a5248fda3cd2610ae7616
Mateusz Łachacz <info@blurbyte.com>
2021-06-07 15:28:04 +0200

    feat: filter out nullish children passed to menu

--- packages/react-components/button/src/UserAccountMenuButton/Menu.tsx
+++ packages/react-components/button/src/UserAccountMenuButton/Menu.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react';
-import { Children } from 'react';
+import { Children, isValidElement } from 'react';
 
 import Button from './Button';
 import Dropdown from './Dropdown';
@@ -25,7 +25,16 @@ function Menu({
   showAlertDot,
   totalXp,
 }: Menu): JSX.Element {
-  const numberOfItems = Children.count(children);
+  // Number of non-null, non-undefined, and non-false children
+  const numberOfItems = Children.toArray(children).length;
+
+  // Render only valid children to keep it in sync with numberOfItems
+  function renderValidChildren(): React.ReactNode {
+    return Children.toArray(children).filter((child) => {
+      return isValidElement(child);
+    }) as React.ReactNode;
+  }
+
   const { buttonProps, isOpen, itemsProps } = useMenu(numberOfItems);
 
   return (
@@ -38,7 +47,7 @@ function Menu({
       />
       {isOpen && (
         <Dropdown itemsProps={itemsProps} totalXp={totalXp}>
-          {children}
+          {renderValidChildren()}
         </Dropdown>
       )}
     </div>
