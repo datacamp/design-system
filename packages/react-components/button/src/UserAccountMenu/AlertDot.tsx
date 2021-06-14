78d6a7e2054965a6390878bfa8ce9a6339c105d0
Mateusz Łachacz <info@blurbyte.com>
2021-06-14 09:26:30 +0200

    feat: update alert dot style

--- packages/react-components/button/src/UserAccountMenu/AlertDot.tsx
+++ packages/react-components/button/src/UserAccountMenu/AlertDot.tsx
@@ -1,19 +1,12 @@
 import wafflesTokens from '@datacamp/waffles-tokens';
 import { css } from '@emotion/react';
 
-import { FULL_MENU_MEDIA_QUERY } from './constants';
-
 const dotStyle = css({
   backgroundColor: wafflesTokens.colors.red,
   border: `1px solid ${wafflesTokens.colors.white}`,
   borderRadius: 8,
-  [FULL_MENU_MEDIA_QUERY]: {
-    display: 'none',
-  },
   height: 8,
   position: 'absolute',
-  right: 0,
-  top: 0,
   width: 8,
   zIndex: 1,
 });
