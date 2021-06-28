074d41bdad7cccfac245fd4e26dfcdf5e46e9fbf
Mateusz Łachacz <info@blurbyte.com>
2021-06-28 12:34:05 +0200

    chore: update unit test

--- packages/other/utils/src/computeDataAttributes.spec.ts
+++ packages/other/utils/src/computeDataAttributes.spec.ts
@@ -1,20 +1,25 @@
 import computeDataAttributes from './computeDataAttributes';
 
 describe('computeDataAttributes', () => {
-  it('returns an empty object when input undefined', () => {
+  it('returns an empty object when input is undefined', () => {
     expect(computeDataAttributes()).toEqual({});
   });
 
-  it('returns an empty object when input an empty object', () => {
+  it('returns an empty object when input is an empty object', () => {
     expect(computeDataAttributes({})).toEqual({});
   });
 
   it('returns an the correct data attributes', () => {
     expect(
-      computeDataAttributes({ test: 'example', test2: 'other example' }),
+      computeDataAttributes({
+        test: 'example',
+        test2: 'other example',
+        testid: 'submit-button',
+      }),
     ).toEqual({
       'data-test': 'example',
       'data-test2': 'other example',
+      'data-testid': 'submit-button',
     });
   });
 });
