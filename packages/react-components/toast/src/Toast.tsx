6c10b23c8693ff9dc61637eeaa91dd08d02ee5dc
Mateusz Łachacz <info@blurbyte.com>
2021-07-05 10:26:23 +0200

    feat: add optional description

--- packages/react-components/toast/src/Toast.tsx
+++ packages/react-components/toast/src/Toast.tsx
@@ -3,10 +3,9 @@ import {
   CheckmarkCircleInvertedIcon,
   CrossCircleInvertedIcon,
 } from '@datacamp/waffles-icons';
-import { Heading } from '@datacamp/waffles-text';
+import { Heading, Paragraph } from '@datacamp/waffles-text';
 import tokens from '@datacamp/waffles-tokens/lib/future-tokens.json';
 import { css } from '@emotion/react';
-import React from 'react';
 
 const colors = {
   error: tokens.color.primary.red.value.hex,
@@ -18,53 +17,57 @@ const icons = {
   success: CheckmarkCircleInvertedIcon,
 };
 
+const wrapperStyle = css({
+  alignItems: 'center',
+  background: 'white',
+  borderRadius: 4,
+  boxShadow:
+    '0px 0px 1px 0px rgba(5, 25, 45, 0.3), 0px 8px 12px -4px rgba(5, 25, 45, 0.3)',
+  display: 'flex',
+  justifyContent: 'space-between',
+  maxWidth: 512,
+  minWidth: 250,
+  position: 'relative',
+  zIndex: parseInt(tokens.zIndex[999].value, 10),
+});
+
+const sideDecorStyle = css({
+  borderRadius: '4px 0px 0px 4px',
+  height: '100%',
+  position: 'absolute',
+  width: 4,
+});
+
 interface ToastProps {
   /**
-   * Used internally. Do not explicitly set
+   * Used internally. Do not explicitly set.
    */
   closeToast?: () => void;
   /**
-   * Determines the colour and icon displayed in the toast.
+   * Optional content to display in the toast below title.
+   */
+  description?: string;
+  /**
+   * Determines the color and icon displayed in the toast.
    */
   intent: 'success' | 'error';
   /**
-   * The main content to display in the toast
+   * The main content to display in the toast.
    */
   title: string;
 }
 
 const Toast = ({
   closeToast,
+  description,
   intent,
   title,
-}: ToastProps): React.ReactElement => {
+}: ToastProps): JSX.Element => {
   const Icon = icons[intent];
   const color = colors[intent];
   return (
-    <div
-      css={css({
-        alignItems: 'center',
-        background: 'white',
-        borderRadius: 4,
-        boxShadow:
-          '0px 0px 1px 0px rgba(5, 25, 45, 0.3), 0px 8px 12px -4px rgba(5, 25, 45, 0.3)',
-        display: 'flex',
-        justifyContent: 'space-between',
-        maxWidth: 512,
-        minWidth: 250,
-        position: 'relative',
-        zIndex: parseInt(tokens.zIndex[999].value, 10),
-      })}
-    >
-      <div
-        css={{
-          backgroundColor: color,
-          borderRadius: '4px 0px 0px 4px',
-          height: '100%',
-          position: 'absolute',
-          width: 4,
-        }}
-      />
+    <div css={wrapperStyle}>
+      <div css={css({ backgroundColor: color }, sideDecorStyle)} />
       <Icon
         color={color}
         css={css({ flexShrink: 0, marginLeft: 16 })}
@@ -74,6 +77,7 @@ const Toast = ({
         <Heading as="h4" multiLine size={300}>
           {title}
         </Heading>
+        {description && <Paragraph>{description}</Paragraph>}
       </div>
       {closeToast && (
         <CloseButton
