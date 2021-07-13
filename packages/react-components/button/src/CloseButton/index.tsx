8d1a03b9d65b522c32debef522393e37d536411d
Mateusz Łachacz <info@blurbyte.com>
2021-07-13 12:05:43 +0200

    feat: update tokens in CloseButton

--- packages/react-components/button/src/CloseButton/index.tsx
+++ packages/react-components/button/src/CloseButton/index.tsx
@@ -1,5 +1,5 @@
 import { CrossIcon } from '@datacamp/waffles-icons';
-import tokens from '@datacamp/waffles-tokens/lib/future-tokens.json';
+import tokens from '@datacamp/waffles-tokens';
 import { css } from '@emotion/react';
 import React from 'react';
 
@@ -13,18 +13,18 @@ const CloseButton: React.FC<{
     aria-label="Close"
     className={className}
     css={css({
-      ':active': { color: tokens.color.primary.green.value.rgb },
+      ':active': { color: tokens.colors.green },
       ':disabled': { color: '#D1D3D8' },
       ':disabled:hover, :disabled:focus': { backgroundColor: 'transparent' },
       ':focus': {
-        backgroundColor: tokens.color.neutral.grey200.value.rgb,
+        backgroundColor: tokens.colors.greyLight,
       },
-      ':hover': { backgroundColor: tokens.color.neutral.grey200.value.rgb },
+      ':hover': { backgroundColor: tokens.colors.greyLight },
       alignItems: 'center',
       backgroundColor: 'white',
       border: 'none',
       borderRadius: 2,
-      color: tokens.color.primary.navyText.value.rgb,
+      color: tokens.colors.navy,
       cursor: disabled ? 'not-allowed' : 'pointer',
       display: 'flex',
       flexDirection: 'column',
