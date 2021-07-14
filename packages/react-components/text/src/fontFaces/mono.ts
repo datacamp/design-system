b41ff837d4cd399a001da1cc4bdb3668004eb554
Mateusz Łachacz <info@blurbyte.com>
2021-07-14 10:38:34 +0200

    feat: upate font face definitions

--- packages/react-components/text/src/fontFaces/mono.ts
+++ packages/react-components/text/src/fontFaces/mono.ts
@@ -1,11 +1,11 @@
 // When editing this file don't forget to also update the font faces in the core package
 
-import tokens from '@datacamp/waffles-tokens/lib/future-tokens.json';
+import tokens from '@datacamp/waffles-tokens';
 import { css } from '@emotion/react';
 
 export default css`
   @font-face {
-    font-family: ${tokens.asset.font.mono.value};
+    font-family: 'JetBrainsMonoNL';
     src: url('https://waffles.datacamp.com/fonts/JetBrainsMono-Regular.woff2')
         format('woff2'),
       url('https://waffles.datacamp.com/fonts/JetBrainsMono-Regular.woff')
@@ -13,6 +13,6 @@ export default css`
       url('https://waffles.datacamp.com/fonts/JetBrainsMonoNL-Regular.ttf')
         format('truetype');
     font-style: 'normal';
-    font-weight: ${tokens.fontWeight.regular.value};
+    font-weight: ${tokens.fontWeights.regular};
   }
 `;
--- packages/react-components/text/src/fontFaces/regular.ts
+++ packages/react-components/text/src/fontFaces/regular.ts
@@ -1,42 +1,42 @@
 // When editing this file don't forget to also update the font faces in the core package
 
-import tokens from '@datacamp/waffles-tokens/lib/future-tokens.json';
+import tokens from '@datacamp/waffles-tokens';
 import { css } from '@emotion/react';
 
 export default css`
   @font-face {
-    font-family: ${tokens.asset.font.sansSerif.value};
+    font-family: 'Studio-Feixen-Sans';
     src: url('https://waffles.datacamp.com/fonts/StudioFeixenSans-Regular.woff2')
         format('woff2'),
       url('https://waffles.datacamp.com/fonts/StudioFeixenSans-Regular.woff')
         format('woff'),
       url('https://waffles.datacamp.com/fonts/StudioFeixenSans-Regular.ttf')
         format('truetype');
-    font-weight: ${tokens.fontWeight.regular.value};
+    font-weight: ${tokens.fontWeights.regular};
     font-display: swap;
   }
 
   @font-face {
-    font-family: ${tokens.asset.font.sansSerif.value};
+    font-family: 'Studio-Feixen-Sans';
     src: url('https://waffles.datacamp.com/fonts/StudioFeixenSans-Bold.woff2')
         format('woff2'),
       url('https://waffles.datacamp.com/fonts/StudioFeixenSans-Bold.woff')
         format('woff'),
       url('https://waffles.datacamp.com/fonts/StudioFeixenSans-Bold.ttf')
         format('truetype');
-    font-weight: ${tokens.fontWeight.bold.value};
+    font-weight: ${tokens.fontWeights.bold};
     font-display: swap;
   }
 
   @font-face {
-    font-family: ${tokens.asset.font.sansSerif.value};
+    font-family: 'Studio-Feixen-Sans';
     src: url('https://waffles.datacamp.com/fonts/StudioFeixenSans-Book.woff2')
         format('woff2'),
       url('https://waffles.datacamp.com/fonts/StudioFeixenSans-Book.woff')
         format('woff'),
       url('https://waffles.datacamp.com/fonts/StudioFeixenSans-Book.ttf')
         format('truetype');
-    font-weight: ${tokens.fontWeight.light.value};
+    font-weight: ${tokens.fontWeights.light};
     font-display: swap;
   }
 `;
