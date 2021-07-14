4ddb53e0cfb08a00958b63cd5eadbe1062b656f2
Mateusz Łachacz <info@blurbyte.com>
2021-07-14 17:51:41 +0200

    feat: update Icons docs

--- packages/docs/next-doc-site/examples/icons/customized.tsx
+++ packages/docs/next-doc-site/examples/icons/customized.tsx
@@ -2,7 +2,7 @@ import { AddCircleIcon } from '@datacamp/waffles-icons';
 import tokens from '@datacamp/waffles-tokens';
 
 function Example(): JSX.Element {
-  return <AddCircleIcon color={tokens.colors.green} size={24} />;
+  return <AddCircleIcon color={tokens.colors.purple} size="small" />;
 }
 
 export default Example;
