4c909bbaaa4772f4084b0c6241e3f94ab2d5b230
Mateusz Łachacz <info@blurbyte.com>
2021-09-09 17:49:03 +0200

    chore: udate tokens helpers unit tests and snapshots

--- packages/other/tokens/test/newTokens.spec.js
+++ packages/other/tokens/test/newTokens.spec.js
@@ -1,14 +1,17 @@
 const {
-  exportForEachTokenGroup,
-  tokensDefaultExport,
-  tokensToEsModule,
   transformedBaseTokens,
   transformedBoxShadows,
   transformedColors,
   transformedFontFamilies,
   transformedFontWeights,
-  transformedLineHeights,
-} = require('../buildNewTokens');
+  transformedPercentages,
+} = require('../newTokensHelpers/transformedBaseTokens');
+const {
+  exportForEachTokenGroup,
+  tokensDefaultExport,
+  tokensToEsModule,
+} = require('../newTokensHelpers/tokensToEsModule');
+
 const baseTokens = require('../base-tokens.json');
 
 const testTokens = {
@@ -57,6 +60,10 @@ const testTokens = {
     relaxed: '150%',
     tight: '100%',
   },
+  opacity: {
+    height: '60%',
+    low: '15%',
+  },
   spacing: {
     large: '24px',
     medium: '16px',
@@ -88,6 +95,10 @@ const transformedTestTokens = {
     relaxed: 1.5,
     tight: 1,
   },
+  opacity: {
+    height: 0.6,
+    low: 0.15,
+  },
   spacing: {
     large: '24px',
     medium: '16px',
@@ -95,43 +106,49 @@ const transformedTestTokens = {
   },
 };
 
-describe('new tokens', () => {
-  it('transformedColors() flattens grouped colors', () => {
-    expect(transformedColors(testTokens.colors)).toEqual(
-      transformedTestTokens.colors,
-    );
+describe('newTokensHelpers', () => {
+  describe('transformedBaseTokens', () => {
+    it('transformedColors() flattens grouped colors', () => {
+      expect(transformedColors(testTokens.colors)).toEqual(
+        transformedTestTokens.colors,
+      );
+    });
+
+    it('transformedBoxShadows() transfroms box shadow from Figma Plugin format to CSS values', () => {
+      expect(transformedBoxShadows(testTokens.boxShadow)).toEqual(
+        transformedTestTokens.boxShadow,
+      );
+    });
+
+    it('transformedFontFamilies() transfroms font families from Figma Plugin format to CSS values', () => {
+      expect(transformedFontFamilies(testTokens.fontFamilies)).toEqual(
+        transformedTestTokens.fontFamilies,
+      );
+    });
+
+    it('transformedFontWeights() transfroms font weights from Figma Plugin format to CSS values', () => {
+      expect(transformedFontWeights(testTokens.fontWeights)).toEqual(
+        transformedTestTokens.fontWeights,
+      );
+    });
+
+    it('transformedPercentages() transfroms line heights and opacity from percanteges to unitless', () => {
+      expect(transformedPercentages(testTokens.lineHeights)).toEqual(
+        transformedTestTokens.lineHeights,
+      );
+      expect(transformedPercentages(testTokens.opacity)).toEqual(
+        transformedTestTokens.opacity,
+      );
+    });
+
+    it('transformedBaseTokens() updates original values to transformed ones', () => {
+      expect(transformedBaseTokens(testTokens)).toEqual(transformedTestTokens);
+    });
   });
 
-  it('transformedBoxShadows() transfroms box shadow from Figma Plugin format to CSS values', () => {
-    expect(transformedBoxShadows(testTokens.boxShadow)).toEqual(
-      transformedTestTokens.boxShadow,
-    );
-  });
-
-  it('transformedFontFamilies() transfroms font families from Figma Plugin format to CSS values', () => {
-    expect(transformedFontFamilies(testTokens.fontFamilies)).toEqual(
-      transformedTestTokens.fontFamilies,
-    );
-  });
-
-  it('transformedFontWeights() transfroms font weights from Figma Plugin format to CSS values', () => {
-    expect(transformedFontWeights(testTokens.fontWeights)).toEqual(
-      transformedTestTokens.fontWeights,
-    );
-  });
-
-  it('transformedLineHeights() transfroms line heights from percanteges to unitless', () => {
-    expect(transformedLineHeights(testTokens.lineHeights)).toEqual(
-      transformedTestTokens.lineHeights,
-    );
-  });
-
-  it('transformedBaseTokens() replaces boxShadow group with transformed one', () => {
-    expect(transformedBaseTokens(testTokens)).toEqual(transformedTestTokens);
-  });
-
-  it('exportForEachTokenGroup() creates ES module export for each token group', () => {
-    const expectedContent = `export const boxShadow = {
+  describe('tokensToEsModule', () => {
+    it('exportForEachTokenGroup() creates ES module export for each token group', () => {
+      const expectedContent = `export const boxShadow = {
   medium: '0px 1px 4px -1px rgba(5, 25, 45, 0.3)',
   thin: '0px 0px 2px 0px rgba(5, 25, 45, 0.3)'
 };
@@ -148,21 +165,24 @@ export const fontFamilies = {
 };
 export const fontWeights = { bold: 800, regular: 400 };
 export const lineHeights = { relaxed: 1.5, tight: 1 };
+export const opacity = { height: 0.6, low: 0.15 };
 export const spacing = { large: '24px', medium: '16px', small: '8px' };
 `;
 
-    expect(exportForEachTokenGroup(transformedTestTokens)).toEqual(
-      expectedContent,
-    );
-  });
+      expect(exportForEachTokenGroup(transformedTestTokens)).toEqual(
+        expectedContent,
+      );
+    });
 
-  it('tokensDefaultExport() creates ES module default export', () => {
-    const expectedContent = `export default { boxShadow, colors, fontFamilies, fontWeights, lineHeights, spacing };`;
-    expect(tokensDefaultExport(transformedTestTokens)).toEqual(expectedContent);
-  });
+    it('tokensDefaultExport() creates ES module default export', () => {
+      const expectedContent = `export default { boxShadow, colors, fontFamilies, fontWeights, lineHeights, opacity, spacing };`;
+      expect(tokensDefaultExport(transformedTestTokens)).toEqual(
+        expectedContent,
+      );
+    });
 
-  it('tokensToEsModule() creates ES module content', () => {
-    const expectedContent = `export const boxShadow = {
+    it('tokensToEsModule() creates ES module content', () => {
+      const expectedContent = `export const boxShadow = {
   medium: '0px 1px 4px -1px rgba(5, 25, 45, 0.3)',
   thin: '0px 0px 2px 0px rgba(5, 25, 45, 0.3)'
 };
@@ -179,10 +199,12 @@ export const fontFamilies = {
 };
 export const fontWeights = { bold: 800, regular: 400 };
 export const lineHeights = { relaxed: 1.5, tight: 1 };
+export const opacity = { height: 0.6, low: 0.15 };
 export const spacing = { large: '24px', medium: '16px', small: '8px' };
-export default { boxShadow, colors, fontFamilies, fontWeights, lineHeights, spacing };`;
+export default { boxShadow, colors, fontFamilies, fontWeights, lineHeights, opacity, spacing };`;
 
-    expect(tokensToEsModule(transformedTestTokens)).toEqual(expectedContent);
+      expect(tokensToEsModule(transformedTestTokens)).toEqual(expectedContent);
+    });
   });
 
   it('base tokens are properly transformed', () => {
