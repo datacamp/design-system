968d17ee2f1a38f0192b6891183ddb1a8a6f3cea
Mateusz Łachacz <info@blurbyte.com>
2021-12-02 15:18:23 +0100

    chore: add unit tests and update snapshots

--- packages/react-components/tooltip/src/index.spec.tsx
+++ packages/react-components/tooltip/src/index.spec.tsx
@@ -1,9 +1,39 @@
+import '@testing-library/jest-dom/extend-expect';
+
 import axeRender from '@datacamp/waffles-axe-render';
 import React from 'react';
 
 import Tooltip from './index';
 
 describe('Tooltip', () => {
+  it('sets the dataAttributes on the tooltip', async () => {
+    const ref = React.createRef<HTMLDivElement>();
+    const { container } = await axeRender(
+      <>
+        <section
+          css={{ height: 50, width: 50 }}
+          data-test-id="target"
+          ref={ref}
+        />
+        <Tooltip
+          appearance="dark"
+          dataAttributes={{ test: 'example' }}
+          id="abc"
+          position="bottom"
+          target={ref}
+          visible
+        >
+          tooltip text content
+        </Tooltip>
+      </>,
+    );
+
+    const tooltipElement = container.querySelector('div');
+
+    expect(tooltipElement).toBeInTheDocument();
+    expect(tooltipElement).toHaveAttribute('data-test', 'example');
+  });
+
   ([
     'bottomLeft',
     'bottom',
