4c67133bf817adc04ca501dff0e63d88dc276181
blurbyte <info@blurbyte.com>
2022-12-08 14:59:27 +0000

    feat: remove CSS font smoothing

--- packages/react-components/text/src/baseStyle.ts
+++ packages/react-components/text/src/baseStyle.ts
@@ -1,7 +1,7 @@
 import tokens from '@datacamp/waffles-tokens/lib/future-tokens.json';
 import { css } from '@emotion/react';
 
-const baseStyle = css('-webkit-font-smoothing: antialiased;', {
+const baseStyle = css({
   color: tokens.color.primary.navyText.value.rgb,
   fontFamily: `${tokens.asset.font.sansSerif.value}, ${tokens.asset.font.sansSerif.attributes.fallback}`,
   fontStyle: 'normal',
